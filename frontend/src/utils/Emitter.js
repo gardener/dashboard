d3a66293ff9aa713023bdab6e5d4a07cbf7914ea
Peter Sutter <peter.sutter@sap.com>
2018-05-17 15:13:02 +0200

    #82 fixed root cause of issue that journal comments were not displayed in some cases this was because in some cases it happened that the subscription for comments was done before the socket connection was authenticated.

--- frontend/src/utils/Emitter.js
+++ frontend/src/utils/Emitter.js
@@ -21,6 +21,190 @@ import Emitter from 'component-emitter'
 import {ThrottledNamespacedEventEmitter} from './ThrottledEmitter'
 import store from '../store'
 
+/* Event Emitters */
+class AbstractEmitter {
+  constructor (socket) {
+    this.authenticated = false
+    this.auth = {
+      bearer: undefined
+    }
+    this.socket = socket
+  }
+
+  authenticate () {
+    console.log(`socket connection ${this.socket.id} authenticating`)
+    if (this.auth.bearer) {
+      this.socket.emit('authentication', this.auth)
+    }
+  }
+
+  onAuthenticated () {
+    this.authenticated = true
+    this.emit('authenticated')
+    console.log(`socket connection ${this.socket.id} authenticated`)
+  }
+
+  onConnect (attempt) {
+    if (attempt) {
+      console.log(`socket connection ${this.socket.id} established after '${attempt}' attempt(s)`)
+    } else {
+      console.log(`socket connection ${this.socket.id} established`)
+    }
+    this.authenticate()
+  }
+
+  onDisconnect (reason) {
+    console.error(`socket connection lost because`, reason)
+    this.authenticated = false
+    this.socket.off('event')
+    this.emit('disconnect', reason)
+  }
+
+  setUser (user) {
+    user = user || {}
+    const id_token = user.id_token
+      /* eslint camelcase: off */
+    if (!id_token) {
+      console.log(`Disconnect socket ${this.socket.id} because ID token is empty`)
+      this.auth.bearer = undefined
+      this.socket.disconnect()
+    } else if (!this.socket.connected) {
+      this.auth.bearer = id_token
+      this.socket.connect()
+    } else if (this.auth.bearer !== id_token) {
+      console.log(`Socket ${this.socket.id} connected but has different ID token`)
+      this.auth.bearer = id_token
+      const onDisconnect = (reason) => {
+        console.log('onDisconnect', reason)
+        if (reason === 'io client disconnect') {
+          clearTimeout(timeoutId)
+          this.socket.connect()
+        }
+      }
+      const onTimeout = () => {
+        this.socket.off('disconnect', onDisconnect)
+      }
+      const timeoutId = setTimeout(onTimeout, 1000)
+      this.socket.once('disconnect', onDisconnect)
+      this.socket.disconnect()
+    }
+  }
+}
+
+class ShootsEmitter extends AbstractEmitter {
+  constructor (socket) {
+    super(socket)
+
+    this.filter = undefined
+    this.namespace = undefined
+  }
+
+  onAuthenticated () {
+    super.onAuthenticated()
+
+    /* currently we only throttle NamespacedEvents (for shoots) as for this kind
+    * we expect many events coming in in a short period of time */
+    const throttledNsEventEmitter = new ThrottledNamespacedEventEmitter({emitter: this, wait: 1000})
+
+    this.socket.on('namespacedEvents', ({kind, namespaces}) => {
+      throttledNsEventEmitter.emit(kind, namespaces)
+    })
+    this.socket.on('batchNamespacedEventsDone', ({kind, namespaces}) => {
+      if (kind === 'shoots') {
+        store.dispatch('unsetShootsLoading', namespaces)
+        throttledNsEventEmitter.flush()
+      }
+    })
+
+    if (this.subscribeAfterAuthentication) {
+      this.subscribe({namespace: this.namespace, filter: undefined})
+      this.subscribeAfterAuthentication = undefined
+    }
+  }
+
+  setNamespace = async function (namespace, filter) {
+    this.namespace = namespace
+    this.filter = filter
+
+    this.subscribe({namespace, filter})
+  }
+
+  subscribe = async function ({namespace, filter}) {
+    if (this.namespace) {
+      if (this.authenticated) {
+        store.dispatch('clearShoots')
+        store.dispatch('setShootsLoading')
+        if (namespace === '_all') {
+          const allNamespaces = await store.getters.namespaces
+          const namespaces = map(allNamespaces, (namespace) => { return {namespace, filter} })
+          this.socket.emit('subscribe', {namespaces})
+        } else if (namespace) {
+          this.socket.emit('subscribe', {namespaces: [{namespace, filter}]})
+        }
+      } else {
+        this.subscribeAfterAuthentication = {namespace, filter}
+      }
+    }
+  }
+}
+
+class JournalsEmitter extends AbstractEmitter {
+  onAuthenticated () {
+    super.onAuthenticated()
+
+    this.socket.on('events', ({kind, events}) => {
+      this.emit(kind, events)
+    })
+
+    this.subscribeIssues()
+
+    if (this.subscribeCommentsAfterAuthentication) {
+      this.subscribeComments(this.subscribeCommentsAfterAuthentication)
+
+      this.subscribeCommentsAfterAuthentication = undefined
+    }
+  }
+
+  setUser (user) {
+    if (!store.getters.isAdmin) {
+      return
+    }
+    super.setUser(user)
+  }
+
+  subscribeIssues () {
+    if (this.authenticated) {
+      if (store.getters.isAdmin) {
+        this.socket.emit('subscribeIssues')
+      }
+    }
+  }
+
+  subscribeComments ({name, namespace}) {
+    if (this.authenticated) {
+      if (store.getters.isAdmin) {
+        this.socket.emit('subscribeComments', {name, namespace})
+        this.subscribedComments = true
+      }
+    } else {
+      this.subscribeCommentsAfterAuthentication = {name, namespace}
+    }
+  }
+
+  unsubscribeComments () {
+    this.subscribeCommentsAfterAuthentication = undefined
+
+    if (this.authenticated) {
+      if (store.getters.isAdmin) {
+        if (this.subscribedComments) {
+          this.socket.emit('unsubscribeComments')
+          this.subscribedComments = false
+        }
+      }
+    }
+  }
+}
+
 const url = window.location.origin
 const socketConfig = {
   path: '/api/events',
@@ -30,165 +214,18 @@ const socketConfig = {
 const shootsSocket = io(`${url}/shoots`, socketConfig)
 const journalsSocket = io(`${url}/journals`, socketConfig)
 
-/* Event Emitters */
-const emitterObjForSocket = (socket) => {
-  return {
-    authenticated: false,
-    namespace: undefined,
-    filter: undefined,
-    auth: {
-      bearer: undefined
-    },
-    socket,
-    setUser (user) {
-      // could be overwritten
-      this._setUser(user)
-    },
-    _setUser (user) {
-      user = user || {}
-      const id_token = user.id_token
-        /* eslint camelcase: off */
-      if (!id_token) {
-        console.log(`Disconnect socket ${this.socket.id} because ID token is empty`)
-        this.auth.bearer = undefined
-        this.socket.disconnect()
-      } else if (!this.socket.connected) {
-        console.log(`Socket ${this.socket.id} not connected.`)
-        this.auth.bearer = id_token
-        this.socket.connect()
-      } else if (this.auth.bearer !== id_token) {
-        console.log(`Socket ${this.socket.id} connected but has different ID token`)
-        this.auth.bearer = id_token
-        const onDisconnect = (reason) => {
-          console.log('ON DISCONNECT')
-          if (reason === 'io client disconnect') {
-            clearTimeout(timeoutId)
-            this.socket.connect()
-          }
-        }
-        const onTimeout = () => {
-          this.socket.off('disconnect', onDisconnect)
-        }
-        const timeoutId = setTimeout(onTimeout, 1000)
-        this.socket.once('disconnect', onDisconnect)
-        this.socket.disconnect()
-      }
-    },
-    setNamespace (namespace, filter) {
-      this.namespace = namespace
-      this.filter = filter
-      store.dispatch('clearShoots')
-      if (this.namespace && this.authenticated) {
-        store.dispatch('setShootsLoading')
-        this.subscribe({namespace, filter})
-      }
-    },
-    authenticate () {
-      console.log(`socket connection ${this.socket.id} authenticating`)
-      if (this.auth.bearer) {
-        this.socket.emit('authentication', this.auth)
-      }
-    },
-    subscribe () {
-      console.log('should be overwritten..', this.socket.id)
-    }
-  }
-}
-
-const shootsEmitterObj = emitterObjForSocket(shootsSocket)
-shootsEmitterObj.subscribe = async function ({namespace, filter}) {
-  if (namespace === '_all') {
-    const allNamespaces = await store.getters.namespaces
-    const namespaces = map(allNamespaces, (namespace) => { return {namespace, filter} })
-    this.socket.emit('subscribe', {namespaces})
-  } else if (namespace) {
-    this.socket.emit('subscribe', {namespaces: [{namespace, filter}]})
-  }
-}
-
-const journalsEmitterObj = emitterObjForSocket(journalsSocket)
-journalsEmitterObj.setUser = function (user) {
-  if (!store.getters.isAdmin) {
-    return
-  }
-  this._setUser(user)
-}
-journalsEmitterObj.subscribe = function () {
-  if (store.getters.isAdmin) {
-    this.socket.emit('subscribeIssues')
-  }
-}
-journalsEmitterObj.subscribeComments = function ({name, namespace}) {
-  if (store.getters.isAdmin) {
-    this.socket.emit('subscribeComments', {name, namespace})
-    this.subscribedComments = true
-  }
-}
-journalsEmitterObj.unsubscribeComments = function () {
-  if (store.getters.isAdmin) {
-    if (this.subscribedComments) {
-      this.socket.emit('unsubscribeComments')
-      this.subscribedComments = false
-    }
-  }
-}
-
-const shootsEmitter = Emitter(shootsEmitterObj)
-const journalsEmitter = Emitter(journalsEmitterObj)
+const shootsEmitter = Emitter(new ShootsEmitter(shootsSocket))
+const journalsEmitter = Emitter(new JournalsEmitter(journalsSocket))
 
 const emitters = [shootsEmitter, journalsEmitter]
-
-function onAuthenticated () {
-  this.authenticated = true
-  this.emit('authenticated')
-  console.log(`socket connection ${this.socket.id} authenticated`)
-
-  this.socket.on('events', ({kind, events}) => {
-    this.emit(kind, events)
-  })
-
-  /* currently we only throttle NamespacedEvents (for shoots) as for this kind
-  * we expect many events coming in in a short period of time */
-  const throttledNsEventEmitter = new ThrottledNamespacedEventEmitter({emitter: this, wait: 1000})
-
-  this.socket.on('namespacedEvents', ({kind, namespaces}) => {
-    throttledNsEventEmitter.emit(kind, namespaces)
-  })
-  this.socket.on('batchNamespacedEventsDone', ({kind, namespaces}) => {
-    if (kind === 'shoots') {
-      store.dispatch('unsetShootsLoading', namespaces)
-      throttledNsEventEmitter.flush()
-    }
-  })
-
-  const namespace = this.namespace
-  const filter = undefined
-  this.subscribe({namespace, filter})
-}
-
-function onConnect (attempt) {
-  if (attempt) {
-    console.log(`socket connection ${this.socket.id} established after '${attempt}' attempt(s)`)
-  } else {
-    console.log(`socket connection ${this.socket.id} established`)
-  }
-  this.authenticate()
-}
-
-function onDisconnect (reason) {
-  console.error(`socket connection lost because`, reason)
-  this.authenticated = false
-  this.socket.off('event')
-  this.emit('disconnect', reason)
-}
 
 /* Web Socket Connection */
 
 forEach(emitters, emitter => {
-  emitter.socket.on('connect', attempt => onConnect.call(emitter, attempt))
-  emitter.socket.on('reconnect', attempt => onConnect.call(emitter, attempt))
-  emitter.socket.on('authenticated', () => onAuthenticated.call(emitter))
-  emitter.socket.on('disconnect', reason => onDisconnect.call(emitter, reason))
+  emitter.socket.on('connect', attempt => emitter.onConnect(attempt))
+  emitter.socket.on('reconnect', attempt => emitter.onConnect(attempt))
+  emitter.socket.on('authenticated', () => emitter.onAuthenticated())
+  emitter.socket.on('disconnect', reason => emitter.onDisconnect(reason))
   emitter.socket.on('connect_error', err => {
     console.error(`socket connection error ${err}`)
   })
