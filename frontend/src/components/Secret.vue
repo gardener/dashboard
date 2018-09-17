7411a4dc2a5240970c15699b1552d2f59769c7aa
Gross, Lukas <lukas.gross@sap.com>
2018-09-17 08:11:15 +0200

    Removed unnecessary slot attribute

--- frontend/src/components/Secret.vue
+++ frontend/src/components/Secret.vue
@@ -46,7 +46,7 @@ limitations under the License.
         @delete="onDelete"
       >
         <!-- forward slot -->
-        <slot slot="rowSubTitle" name="rowSubTitle" :secret="row"></slot>
+        <slot name="rowSubTitle" :secret="row"></slot>
       </secret-row>
     </v-list>
 
