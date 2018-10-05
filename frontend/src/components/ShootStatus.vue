a300d97fe29e1a9a6f821cb55866f6188013d34a
Peter Sutter <peter.sutter@sap.com>
2018-10-05 17:10:37 +0200

    prioritize hibernation status icon

--- frontend/src/components/ShootStatus.vue
+++ frontend/src/components/ShootStatus.vue
@@ -23,18 +23,19 @@ limitations under the License.
             <v-icon v-if="isUserError" class="cursor-pointer progress-icon-user-error" color="error">mdi-account-alert</v-icon>
             <v-icon v-else-if="shootDeleted" class="cursor-pointer progress-icon" :color="color">mdi-delete</v-icon>
             <v-icon v-else-if="isTypeCreate" class="cursor-pointer progress-icon" :color="color">mdi-plus</v-icon>
+            <v-icon v-else-if="isHibernated" class="cursor-pointer progress-icon" :color="color">mdi-sleep</v-icon>
             <span v-else-if="isTypeReconcile && isError" class="error-exclamation-mark">!</span>
             <v-icon v-else-if="isTypeReconcile" class="cursor-pointer progress-icon-check" :color="color">mdi-check</v-icon>
             <span v-else-if="isError" class="error-exclamation-mark">!</span>
             <template v-else>{{operation.progress}}</template>
           </v-progress-circular>
+          <v-icon v-else-if="isHibernated" class="cursor-pointer status-icon" :color="color">mdi-sleep</v-icon>
           <v-icon v-else-if="reconciliationDeactivated" class="cursor-pointer status-icon" :color="color">mdi-block-helper</v-icon>
           <v-icon v-else-if="isAborted && shootDeleted" class="cursor-pointer status-icon" :color="color">mdi-delete</v-icon>
           <v-icon v-else-if="isAborted && isTypeCreate" class="cursor-pointer status-icon" :color="color">mdi-plus</v-icon>
           <v-icon v-else-if="isUserError" class="cursor-pointer status-icon" :color="color">mdi-account-alert</v-icon>
           <v-icon v-else-if="isError" class="cursor-pointer status-icon" :color="color">mdi-alert-outline</v-icon>
           <v-progress-circular v-else-if="isPending" class="cursor-pointer" :size="27" :width="3" indeterminate :color="color"></v-progress-circular>
-          <v-icon v-else-if="isHibernated" class="cursor-pointer status-icon" :color="color">mdi-sleep</v-icon>
           <v-icon v-else class="cursor-pointer status-icon-check" :color="color">mdi-check-circle-outline</v-icon>
         </template>
         <div>{{ tooltipText }}</div>
