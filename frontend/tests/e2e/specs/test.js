548675e9ad6d0374a728e98b49510517a19b930f
Peter Sutter <peter.sutter@sap.com>
2018-10-16 15:04:12 +0200

    fix unit tests

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -3,6 +3,21 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.contains('h1', 'Gardener')
+    cy.contains('h2', 'The Kubernetes Botanist')
+    cy.get('img.logo').should('have.length', 1)
+    cy.get('.loginContainer').should('exist')
+
+    // .click('.loginButton', assertClickStatus)
+    // .waitForElementVisible('input[type=password]', 5 * 1000)
+    // .setValue('input[type=text]', 'nightwatch@example.org')
+    // .setValue('input[type=password]', 'secret')
+    // .click('button[type=submit]', assertClickStatus)
+    // .waitForElementVisible('main h3', 5 * 1000)
+    // .assert.containsText('main h3', 'Let\'s get started')
+    // .assert.elementPresent('nav a[href="/account"]')
+    // .click('nav a[href="/account"]', assertClickStatus)
+    // .waitForElementVisible('i.mdi-account', 5 * 1000)
+    // .assert.containsText('.flex.xs5.offset-xs1 > p', 'nightwatch@example.org')
   })
 })
--- frontend/tests/unit/CodeBlock.spec.js
+++ frontend/tests/unit/CodeBlock.spec.js
@@ -1,5 +1,21 @@
+//
+// Copyright (c) 2018 by SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import CodeBlock from '@/components/CodeBlock.vue'
 import VIcon from 'vuetify/es5/components/VIcon'
 import VBtn from 'vuetify/es5/components/VBtn'
@@ -13,7 +29,7 @@ describe('CodeBlock.vue', function () {
         foo: true
         bar: 42`
     }
-    const wrapper = shallowMount(CodeBlock, {
+    const wrapper = mount(CodeBlock, {
       propsData,
       components: {
         VIcon,
