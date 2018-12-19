76961a8ee0cba037d902d0e48608f7b79352fdf0
Holger Koser <holger.koser@sap.com>
2018-12-19 14:51:26 +0100

    Enable alicloud infrastructure

--- frontend/src/dialogs/SecretDialog.vue
+++ frontend/src/dialogs/SecretDialog.vue
@@ -25,7 +25,8 @@ limitations under the License.
         <v-container fill-height>
           <v-layout align-center justify-start row fill-height>
             <v-flex xs1>
-              <v-icon x-large class="white--text infra_icon">{{infraIcon}}</v-icon>
+              <img v-if="infraIcon === 'alicloud'" src="@/assets/alicloud-white.svg" width="42">
+              <v-icon v-else x-large class="white--text infra_icon">{{infraIcon}}</v-icon>
             </v-flex>
             <v-flex>
               <div class="credential_title">{{title}}</div>
---
+++ frontend/src/dialogs/SecretDialogAlicloud.vue
@@ -0,0 +1,171 @@
+<!--
+Copyright (c) 2018 by SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ -->
+
+<template>
+  <secret-dialog
+    :value=value
+    :data="secretData"
+    :dataValid="valid"
+    :secret="secret"
+    cloudProviderKind="alicloud"
+    color="grey darken-4"
+    infraIcon="alicloud"
+    backgroundSrc="/static/background_alicloud.svg"
+    createTitle="Add new Alibaba Cloud Secret"
+    replaceTitle="Replace Alibaba Cloud Secret"
+    @input="onInput">
+
+    <template slot="data-slot">
+      <v-layout row>
+        <v-flex xs8>
+          <v-text-field
+            color="grey darken-4"
+            ref="accessKeyId"
+            v-model="accessKeyId"
+            :label="accessKeyIdLabel"
+            :error-messages="getErrorMessages('accessKeyId')"
+            @input="$v.accessKeyId.$touch()"
+            @blur="$v.accessKeyId.$touch()"
+            counter="128"
+            hint="e.g. AKIAIOSFODNN7EXAMPLE"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row>
+        <v-flex xs8>
+          <v-text-field
+            color="grey darken-4"
+            v-model="accessKeySecret"
+            :label="accessKeySecretLabel"
+            :error-messages="getErrorMessages('accessKeySecret')"
+            :append-icon="hideSecret ? 'visibility' : 'visibility_off'"
+            :type="hideSecret ? 'password' : 'text'"
+            @click:append="() => (hideSecret = !hideSecret)"
+            @input="$v.accessKeySecret.$touch()"
+            @blur="$v.accessKeySecret.$touch()"
+            hint="e.g. wJalrXUtnFEMIK7MDENG/bPxRfiCYzEXAMPLEKEY"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </template>
+
+  </secret-dialog>
+
+</template>
+
+<script>
+import SecretDialog from '@/dialogs/SecretDialog'
+import { required, minLength, maxLength } from 'vuelidate/lib/validators'
+import { alphaNumUnderscore, base64 } from '@/utils/validators'
+import { getValidationErrors, setDelayedInputFocus } from '@/utils'
+
+const validationErrors = {
+  accessKeyId: {
+    required: 'You can\'t leave this empty.',
+    minLength: 'It must contain at least 16 characters.',
+    maxLength: 'It exceeds the maximum length of 128 characters.'
+  },
+  accessKeySecret: {
+    required: 'You can\'t leave this empty.'
+  }
+}
+
+export default {
+  components: {
+    SecretDialog
+  },
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    },
+    secret: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      accessKeyId: undefined,
+      accessKeySecret: undefined,
+      hideSecret: true,
+      validationErrors
+    }
+  },
+  validations () {
+    // had to move the code to a computed property so that the getValidationErrors method can access it
+    return this.validators
+  },
+  computed: {
+    valid () {
+      return !this.$v.$invalid
+    },
+    secretData () {
+      return {
+        accessKeyID: this.accessKeyId,
+        accessKeySecret: this.accessKeySecret
+      }
+    },
+    validators () {
+      const validators = {
+        accessKeyId: {
+          required,
+          minLength: minLength(16),
+          maxLength: maxLength(128)
+        },
+        accessKeySecret: {
+          required
+        }
+      }
+      return validators
+    },
+    isCreateMode () {
+      return !this.secret
+    },
+    accessKeyIdLabel () {
+      return this.isCreateMode ? 'Access Key Id' : 'New Access Key Id'
+    },
+    accessKeySecretLabel () {
+      return this.isCreateMode ? 'Access Key Secret ' : 'New Access Key Secret'
+    }
+  },
+  methods: {
+    onInput (value) {
+      this.$emit('input', value)
+    },
+    reset () {
+      this.$v.$reset()
+
+      this.accessKeyId = ''
+      this.accessKeySecret = ''
+
+      if (!this.isCreateMode) {
+        setDelayedInputFocus(this, 'accessKeyId')
+      }
+    },
+    getErrorMessages (field) {
+      return getValidationErrors(this, field)
+    }
+  },
+  watch: {
+    value: function (value) {
+      if (value) {
+        this.reset()
+      }
+    }
+  }
+}
+</script>
