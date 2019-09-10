6cfaa14c9fcabb9d4bdf8fda420df880c1df62f2
Lukas Gross <lukas.gross@sap.com>
2019-09-10 14:02:16 +0200

    update disclaimer text

--- frontend/src/dialogs/SecretDialog.vue
+++ frontend/src/dialogs/SecretDialog.vue
@@ -69,7 +69,7 @@ limitations under the License.
 
           <slot name="data-slot"></slot>
           <alert color="error" :message.sync="errorMessage" :detailedMessage.sync="detailedErrorMessage"></alert>
-          <p class="subheading mt-3">Do not use personalized login credentials. Instead, use the credentials of a technical user.</p>
+          <p class="subheading mt-3">Do not use personalized login credentials. Instead, use credentials of a technical user.</p>
         </v-container>
       </v-card-text>
       <v-alert :value="!isCreateMode && relatedShootCount > 0" type="warning">
