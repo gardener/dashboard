edda0d020a321fa03b564810626bc859b6db408a
Lukas Gross <lukas.gross@sap.com>
2019-09-25 16:09:09 +0200

    rm yaml editor autocomplete and tooltip feature

--- frontend/src/components/ShootEditor.vue
+++ frontend/src/components/ShootEditor.vue
@@ -73,17 +73,6 @@ limitations under the License.
         <slot name="toolbarItemsRight"></slot>
       </v-layout>
     </v-flex>
-    <v-tooltip
-      right
-      absolute
-      :value="helpTooltip.visible"
-      :position-x="helpTooltip.posX"
-      :position-y="helpTooltip.posY"
-      max-width="800px"
-      >
-      <span class="font-weight-bold">{{helpTooltip.property}}</span><span class="font-style-italic ml-2">{{helpTooltip.type}}</span><br />
-      <span class="font-wrap">{{helpTooltip.description}}</span>
-    </v-tooltip>
     <v-snackbar v-model="snackbar" top absolute :color="snackbarColor" :timeout="snackbarTimeout">
       {{ snackbarText }}
     </v-snackbar>
@@ -95,7 +84,6 @@ import CopyBtn from '@/components/CopyBtn'
 import GAlert from '@/components/GAlert'
 import { mapState } from 'vuex'
 import { getProjectName } from '@/utils'
-import { getShootSpec } from '@/utils/api'
 import download from 'downloadjs'
 
 // codemirror
@@ -111,20 +99,6 @@ import get from 'lodash/get'
 import omit from 'lodash/omit'
 import cloneDeep from 'lodash/cloneDeep'
 import assign from 'lodash/assign'
-import forIn from 'lodash/forIn'
-import flatMap from 'lodash/flatMap'
-import map from 'lodash/map'
-import reverse from 'lodash/reverse'
-import trim from 'lodash/trim'
-import nth from 'lodash/nth'
-import forEach from 'lodash/forEach'
-import filter from 'lodash/filter'
-import includes from 'lodash/includes'
-import last from 'lodash/last'
-import words from 'lodash/words'
-import join from 'lodash/join'
-import repeat from 'lodash/repeat'
-import upperFirst from 'lodash/upperFirst'
 
 // js-yaml
 import jsyaml from 'js-yaml'
@@ -173,16 +147,7 @@ export default {
       },
       generation: undefined,
       lineHeight: 21,
-      toolbarHeight: 48,
-      shootCompletions: {},
-      helpTooltip: {
-        visible: false,
-        posX: 0,
-        posY: 0,
-        property: undefined,
-        type: undefined,
-        description: undefined
-      }
+      toolbarHeight: 48
     }
   },
   computed: {
@@ -296,8 +261,7 @@ export default {
         },
         'Shift-Tab': (instance) => {
           instance.indentSelection('subtract')
-        },
-        'Ctrl-Space': 'autocomplete'
+        }
       }, this.extraKeys)
       const options = {
         mode: 'text/x-yaml',
@@ -322,192 +286,6 @@ export default {
         this.detailedErrorMessage = undefined
       }
       this.$instance.on('change', onChange)
-
-      const yamlHint = (editor, options) => {
-        const cur = editor.getCursor()
-        const token = getYamlToken(editor, cur)
-        const list = getYamlCompletions(token, cur, editor)
-
-        options.completeSingle = false
-
-        return {
-          list,
-          from: CodeMirror.Pos(cur.line, token.start),
-          to: CodeMirror.Pos(cur.line, token.end)
-        }
-      }
-
-      const getYamlCompletions = (token, cur, editor, exactMatch = false) => {
-        let tprop = token
-        let line = cur.line
-        let context = []
-        while (line > 0 && !(tprop.type === 'property' && tprop.indent === 0)) {
-          tprop = getYamlToken(editor, CodeMirror.Pos(line, 0))
-          if (tprop.indent < token.start &&
-              (context.length === 0 || context[context.length - 1].indent > tprop.indent)) {
-            if (tprop.type === 'property') {
-              context.push(tprop)
-            } else if (tprop.type === 'arrayStart') {
-              context.push(tprop)
-            }
-          }
-          line--
-        }
-
-        return getCompletions(token, context, exactMatch)
-      }
-
-      const getYamlToken = (editor, cur) => {
-        let token
-        let lineTokens = editor.getLineTokens(cur.line)
-        const lineString = join(map(lineTokens, 'string'), '')
-        forEach(lineTokens, lineToken => {
-          let result = lineToken.string.match(/^(\s*)-\s(.*)?$/)
-          if (result) {
-            token = lineToken
-            token.type = 'arrayStart'
-            token.string = trim(last(words(lineString.substring(0, cur.ch).toLowerCase())))
-            token.indent = token.start = result[1].length
-            token.end = token.start + token.string.length + 2
-            if (lineTokens.length >= 2 && lineTokens[lineTokens.length - 1].string === ':') {
-              // arrayStart line can also start new object
-              let result = lineTokens[lineTokens.length - 2].string.match(/^(\s*)(.+?)$/)
-              token.propertyName = result[2]
-            }
-          }
-        })
-        if (!token) {
-          if (lineTokens.length >= 2 && lineTokens[lineTokens.length - 1].string === ':') {
-            token = lineTokens[lineTokens.length - 2]
-            let result = token.string.match(/^(\s*)(.+?)$/)
-            token.indent = token.start = result[1].length
-            token.propertyName = result[2]
-            token.type = 'property'
-          }
-        }
-        if (!token) {
-          token = editor.getTokenAt(cur, true)
-          token.string = trim(last(words(lineString.substring(0, cur.ch).toLowerCase())))
-          token.start = token.end - token.string.length
-        }
-        return token
-      }
-
-      const getCompletions = (token, context, exactMatch) => {
-        const completionPath = flatMap(reverse(context), (pathToken, index, revContext) => {
-          if (pathToken.type === 'property') {
-            if (get(nth(revContext, index + 1), 'type') === 'arrayStart') {
-              // next item is array, so don't append 'properties'
-              return pathToken.propertyName
-            }
-            if (index === revContext.length - 1 && token.type === 'arrayStart') {
-              // current token is array, so list properties of its items
-              return [pathToken.propertyName, 'items', 'properties']
-            }
-            // normal property token
-            return [pathToken.propertyName, 'properties']
-          } else if (pathToken.type === 'arrayStart') {
-            if (pathToken.propertyName !== undefined && token.start === pathToken.indent + (this.$instance.options.indentUnit * 2)) {
-              // arrayStart line can also start new object, so list properties of specific item
-              return ['items', 'properties', pathToken.propertyName, 'properties']
-            }
-            // path token is array, so list properties of its items
-            return ['items', 'properties']
-          } else {
-            return []
-          }
-        })
-
-        let completions
-        if (completionPath.length > 0) {
-          completions = get(this.shootCompletions, completionPath)
-        } else {
-          completions = this.shootCompletions
-        }
-
-        let completionArray = []
-        const generateCompletionText = (key, type) => {
-          const completionIndentStr = `${repeat(' ', token.start)}${repeat(' ', this.$instance.options.indentUnit)}`
-          if (type === 'array') {
-            return `${key}:\n${completionIndentStr}- `
-          } else if (type === 'object') {
-            return `${key}:\n${completionIndentStr}`
-          } else {
-            return `${key}: `
-          }
-        }
-        forIn(completions, (value, key) => {
-          const text = `${token.type === 'arrayStart' ? '- ' : ''}${generateCompletionText(key, value.type)}`
-          const string = key.toLowerCase()
-          completionArray.push({
-            text,
-            string,
-            property: key,
-            type: upperFirst(value.type),
-            description: value.description,
-            render: (el, self, data) => {
-              const propertyWrapper = document.createElement('div')
-              propertyWrapper.innerHTML = `<span class="property">${key}</span><span class="type">${upperFirst(value.type)}</span>`
-              propertyWrapper.className = 'ghint-type'
-              el.appendChild(propertyWrapper)
-
-              const descWrapper = document.createElement('div')
-              descWrapper.innerHTML = `<span class="description">${value.description}</span>`
-              descWrapper.className = 'ghint-desc'
-              el.appendChild(descWrapper)
-            }
-          })
-        })
-        if (trim(token.string).length > 0) {
-          completionArray = filter(completionArray, completion => {
-            if (exactMatch) {
-              return isEqual(completion.string, token.string)
-            }
-            return includes(completion.string, token.string)
-          })
-        }
-        return completionArray
-      }
-
-      CodeMirror.registerHelper('hint', 'yaml', yamlHint)
-
-      const cm = this.$instance
-      let cmTooltipFnTimerID
-      const cmTooltipFn = (e) => {
-        let pos = cm.coordsChar({
-          left: e.clientX,
-          top: e.clientY
-        })
-        const lineTokens = cm.getLineTokens(pos.line)
-        const lineString = join(map(lineTokens, 'string'), '')
-        const result = lineString.match(/^(\s*)(.*?)([^\s]+):.*$/)
-        if (result) {
-          const start = result[1].length + result[2].length
-          const end = start + result[3].length
-          const string = result[3].toLowerCase()
-          const token = {
-            start,
-            end,
-            string
-          }
-          if (token.start <= pos.ch && pos.ch <= token.end) {
-            const completions = getYamlCompletions(token, pos, cm, true)
-            if (completions.length === 1) {
-              this.helpTooltip.visible = true
-              this.helpTooltip.posX = e.clientX
-              this.helpTooltip.posY = e.clientY
-              this.helpTooltip.property = completions[0].property
-              this.helpTooltip.type = completions[0].type
-              this.helpTooltip.description = completions[0].description
-            }
-          }
-        }
-      }
-      CodeMirror.on(element, 'mouseover', (e) => {
-        clearTimeout(cmTooltipFnTimerID)
-        this.helpTooltip.visible = false
-        cmTooltipFnTimerID = setTimeout(cmTooltipFn, 800, e)
-      })
     },
     destroyInstance () {
       if (this.$instance) {
@@ -570,8 +348,6 @@ export default {
     this.createInstance(this.$refs.container)
     this.update(this.value)
     this.refresh()
-    const shootCompletions = await getShootSpec()
-    this.shootCompletions = get(shootCompletions, 'data.spec.properties', {})
   },
   watch: {
     value: {
