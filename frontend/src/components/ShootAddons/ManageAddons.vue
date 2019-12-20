9f0ffabcea8dafa66b3a6d957306549249277fba
Lukas Gross <lukas.gross@sap.com>
2019-12-20 13:06:04 +0100

    Fixed: Addon dialog not resetted properly Fixed: Vendor icon error in console for unknown infrastructure providers

--- frontend/src/components/ShootAddons/ManageAddons.vue
+++ frontend/src/components/ShootAddons/ManageAddons.vue
@@ -67,7 +67,7 @@ export default {
     },
     updateAddons (addons) {
       this.resetAddonList(addons)
-      assign(this.addons, cloneDeep(addons))
+      this.addons =  cloneDeep(addons)
     },
     resetAddonList (addons) {
       this.addonDefinitionList = filter(shootAddonList, addon => {
