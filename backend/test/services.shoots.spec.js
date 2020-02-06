db29a00993212a18adaa6dc91f0f5e05fc5f3d9a
Peter Sutter <peter.sutter@sap.com>
2020-02-06 09:35:15 +0100

    fix typo and lint error

--- backend/test/services.shoots.spec.js
+++ backend/test/services.shoots.spec.js
@@ -17,28 +17,28 @@
 'use strict'
 
 const {
-  getDashboarUrlPath
+  getDashboardUrlPath
 } = require('../lib/services/shoots')
 
 describe('services', function () {
   /* eslint no-unused-expressions: 0 */
   describe('shoots', function () {
-    describe('#dashboarUrlPath', function () {
+    describe('#getDashboardUrlPath', function () {
       it('should return dashboard URL path for kubernetes version < 1.16.0', async function () {
-        expect(getDashboarUrlPath('1.14.0')).to.be.equals('/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/')
-        expect(getDashboarUrlPath('1.15.1')).to.be.equals('/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/')
-        expect(getDashboarUrlPath('1.15.9')).to.be.equals('/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('1.14.0')).to.be.equals('/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('1.15.1')).to.be.equals('/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('1.15.9')).to.be.equals('/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/')
       })
 
       it('should return dashboard URL path for kubernetes version >= 1.16.0', async function () {
-        expect(getDashboarUrlPath('1.16.0')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
-        expect(getDashboarUrlPath('1.16.1')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
-        expect(getDashboarUrlPath('1.17.0')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
-        expect(getDashboarUrlPath('v1.17.0')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('1.16.0')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('1.16.1')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('1.17.0')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
+        expect(getDashboardUrlPath('v1.17.0')).to.be.equals('/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/')
       })
 
       it('should return no dashboard URL path', async function () {
-        expect(getDashboarUrlPath(undefined)).to.be.undefined
+        expect(getDashboardUrlPath(undefined)).to.be.undefined
       })
     })
   })
