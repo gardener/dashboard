bb67acf5523f53b536c30117d29451a520c7e229
Lukas Gross <lukas.gross@sap.com>
2020-02-18 12:12:30 +0100

    Fixed UI tests

--- frontend/tests/unit/NewShootDetails.spec.js
+++ frontend/tests/unit/NewShootDetails.spec.js
@@ -15,7 +15,7 @@
 //
 
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import NewShootDetails from '@/components/NewShoot/NewShootDetails.vue'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
@@ -44,7 +44,7 @@ function createNewShootDetailsComponent (kubernetesVersion) {
   const propsData = {
     userInterActionBus: new EventEmitter()
   }
-  const wrapper = shallowMount(NewShootDetails, {
+  const wrapper = mount(NewShootDetails, {
     propsData,
     computed: {
       sortedKubernetesVersionsList: () => sampleVersions
