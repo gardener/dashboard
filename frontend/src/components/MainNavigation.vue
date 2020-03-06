81fbc5b1d957a359131363fe8fd6ca4be73f0172
Lukas Gross <lukas.gross@sap.com>
2020-03-06 08:15:23 +0100

    Implemented exact match feature for project list search Also fixed arrow navigation

--- frontend/src/components/MainNavigation.vue
+++ frontend/src/components/MainNavigation.vue
@@ -36,82 +36,96 @@ limitations under the License.
         </div>
       </div>
       <template v-if="projectList.length">
-      <v-menu
-        light
-        attach
-        offset-y
-        left
-        bottom
-        full-width
-        allow-overflow
-        open-on-click
-        :close-on-content-click="false"
-        content-class="project-menu"
-        v-model="projectMenu"
-      >
-        <v-btn block slot="activator" class="project-selector elevation-4 ma-0 white--text">
-          <v-icon class="pr-4">mdi-grid-large</v-icon>
-          <span class="ml-2">{{projectName}}</span>
-          <v-spacer></v-spacer>
-          <v-icon right>{{projectMenuIcon}}</v-icon>
-        </v-btn>
+        <v-menu
+          light
+          attach
+          offset-y
+          left
+          bottom
+          full-width
+          allow-overflow
+          open-on-click
+          :close-on-content-click="false"
+          content-class="project-menu"
+          v-model="projectMenu"
+        >
+          <v-btn
+            block
+            slot="activator"
+            class="project-selector elevation-4 ma-0 white--text"
+            @keydown.down="selectProjectWithKeys('down')"
+            @keydown.up="selectProjectWithKeys('up')"
+          >
+            <v-icon class="pr-4">mdi-grid-large</v-icon>
+            <span class="ml-2">{{projectName}}</span>
+            <v-spacer></v-spacer>
+            <v-icon right>{{projectMenuIcon}}</v-icon>
+          </v-btn>
 
-        <v-card light>
-          <template v-if="projectList.length > 3">
-            <v-card-title class="pa-0 grey lighten-5">
-              <v-text-field
-                light
-                clearable
-                label="Filter projects"
-                single-line
-                hide-details
-                full-width
-                color="grey darken-1"
-                prepend-icon="search"
-                class="ml-4 project-filter"
-                v-model="projectFilter"
-                ref="projectFilter"
-                @keyup.esc="projectFilter = ''"
-                @keyup.enter="onProjectFilterSubmit()"
-                autofocus
+          <v-card light>
+            <template v-if="projectList.length > 3">
+              <v-card-title class="pa-0 grey lighten-5">
+                <v-text-field
+                  light
+                  clearable
+                  label="Filter projects"
+                  single-line
+                  hide-details
+                  full-width
+                  color="grey darken-1"
+                  prepend-icon="search"
+                  class="ml-4 project-filter"
+                  v-model="projectFilter"
+                  ref="projectFilter"
+                  @keyup.esc="projectFilter = ''"
+                  @keyup.enter="onEnterProjectFilter"
+                  @input="onInputProjectFilter"
+                  @keydown.down="selectProjectWithKeys('down')"
+                  @keydown.up="selectProjectWithKeys('up')"
+                  autofocus
+                >
+                </v-text-field>
+              </v-card-title>
+              <v-divider></v-divider>
+            </template>
+            <v-list light class="project-list" ref="projectList">
+              <v-list-tile
+                class="project-list-tile"
+                v-for="project in sortedAndFilteredProjectListWithAllProjects"
+                @click="onProjectClick($event, project)"
+                :class="{'grey lighten-4' : selectedProject === project}"
+                :key="project.metadata.name"
               >
-              </v-text-field>
-            </v-card-title>
-            <v-divider></v-divider>
-          </template>
-          <v-list light class="project-list">
-            <v-list-tile
-              v-for="project in sortedAndFilteredProjectList"
-              :key="project.metadata.name"
-              @click="onProjectSelect(project)"
-            >
-              <v-list-tile-avatar>
-                <v-icon v-if="project.metadata.name===projectName" color="teal">check</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>{{project.metadata.name}}</v-list-tile-title>
-                <v-list-tile-sub-title class="project-owner">{{getProjectOwner(project)}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-card-actions class="grey lighten-3">
-            <v-tooltip top :disabled="canCreateProject" style="width: 100%">
-              <v-btn
-                slot="activator"
-                flat
-                block
-                class="project-add text-xs-left teal--text"
-                :disabled="!canCreateProject"
-                @click.stop="openProjectDialog"
-              >
-                <v-icon>add</v-icon>
-                <span class="ml-2">Create Project</span>
-              </v-btn>
-              <span>You are not authorized to create projects</span>
-            </v-tooltip>
-          </v-card-actions>
-        </v-card>
-      </v-menu>
+                <v-list-tile-avatar>
+                  <v-icon v-if="project.metadata.name === projectName" color="teal">check</v-icon>
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{project.metadata.name}}</v-list-tile-title>
+                  <v-list-tile-sub-title class="project-owner">{{getProjectOwner(project)}}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action v-if="project.metadata.name === projectFilter">
+                  <v-chip small color="cyan darken-2">Exact match</v-chip>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-list>
+            <v-card-actions class="grey lighten-3">
+              <v-tooltip top :disabled="canCreateProject" style="width: 100%">
+                <v-btn
+                  slot="activator"
+                  flat
+                  block
+                  class="project-add text-xs-left teal--text"
+                  :disabled="!canCreateProject"
+                  @click.stop="openProjectDialog"
+                >
+                  <v-icon>add</v-icon>
+                  <span class="ml-2">Create Project</span>
+                </v-btn>
+                <span>You are not authorized to create projects</span>
+              </v-tooltip>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
       </template>
       <v-list>
         <v-list-tile :to="{name: 'Home'}" exact v-if="hasNoProjects">
@@ -151,6 +165,7 @@ limitations under the License.
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex'
 import find from 'lodash/find'
+import findIndex from 'lodash/findIndex'
 import filter from 'lodash/filter'
 import sortBy from 'lodash/sortBy'
 import toLower from 'lodash/toLower'
@@ -173,7 +188,8 @@ export default {
       projectDialog: false,
       projectFilter: '',
       projectMenu: false,
-      allProjectsItem: { metadata: { name: 'All Projects', namespace: '_all' } }
+      allProjectsItem: { metadata: { name: 'All Projects', namespace: '_all' } },
+      selectedProjectIndex: 0
     }
   },
   computed: {
@@ -240,11 +256,19 @@ export default {
         const owner = toLower(replace(item.data.owner, /@.*$/, ''))
         return includes(name, filter) || includes(owner, filter)
       }
-      const sortedList = sortBy(filter(this.projectList, predicate))
-      if (sortedList.length > 1) {
-        sortedList.unshift(this.allProjectsItem)
+      const filteredList = filter(this.projectList, predicate)
+
+      const exactMatch = item => {
+        return item.metadata.name === this.projectFilter ? 0 : 1
       }
+      const sortedList = sortBy(filteredList, exactMatch, 'metadata.name')
       return sortedList
+    },
+    sortedAndFilteredProjectListWithAllProjects () {
+      if (this.projectList.length > 1) {
+        return [this.allProjectsItem, ...this.sortedAndFilteredProjectList]
+      }
+      return this.sortedAndFilteredProjectList
     },
     getProjectOwner () {
       return (project) => {
@@ -255,22 +279,28 @@ export default {
       return (route) => {
         return namespacedRoute(route, this.namespace)
       }
+    },
+    projectFilterHasExactMatch () {
+      return get(head(this.sortedAndFilteredProjectList), 'metadata.name') === this.projectFilter
+    },
+    selectedProject () {
+      return this.sortedAndFilteredProjectListWithAllProjects[this.selectedProjectIndex]
     }
   },
   methods: {
     ...mapActions([
       'setSidebar'
     ]),
-    onProjectSelect (project) {
+    onEnterProjectFilter () {
       this.projectMenu = false
-      this.project = project
+      this.project = this.selectedProject
     },
-    onProjectFilterSubmit () {
-      if (this.projectMenu && this.sortedAndFilteredProjectList.length === 1) {
-        const project = head(this.sortedAndFilteredProjectList)
-        if (project) {
-          this.onProjectSelect(project)
-        }
+    onProjectClick (event, project) {
+      this.projectMenu = false
+      if (event.isTrusted) {
+        this.project = project
+      } else { // open item selected via keys
+        this.project = this.selectedProject
       }
     },
     openProjectDialog () {
@@ -292,6 +322,30 @@ export default {
         name = fallback
       }
       return !this.namespaced ? { name, query: { namespace } } : { name, params: { namespace } }
+    },
+    onInputProjectFilter () {
+      this.selectedProjectIndex = 0
+      if (this.projectFilterHasExactMatch) {
+        this.selectedProjectIndex = findIndex(this.sortedAndFilteredProjectListWithAllProjects, { metadata: { name: this.projectFilter } })
+      } else if (this.sortedAndFilteredProjectList.length === 1) {
+        const project = head(this.sortedAndFilteredProjectList)
+        this.selectedProjectIndex = findIndex(this.sortedAndFilteredProjectListWithAllProjects, project)
+      }
+    },
+    selectProjectWithKeys (keyDirection) {
+      if (keyDirection === 'up') {
+        if (this.selectedProjectIndex > 0) {
+          this.selectedProjectIndex--
+        }
+      } else if (keyDirection === 'down') {
+        if (this.selectedProjectIndex < this.sortedAndFilteredProjectListWithAllProjects.length - 1) {
+          this.selectedProjectIndex++
+        }
+      }
+      const el = get(this.$refs.projectList.$children[this.selectedProjectIndex], '$el')
+      if (el) {
+        el.scrollIntoView(false)
+      }
     }
   },
   watch: {
@@ -428,6 +482,12 @@ export default {
           color: white !important
         }
       }
+    }
+  }
+
+  .project-list {
+    >>>.v-list__tile--highlighted {
+      background-color: transparent !important
     }
   }
 
