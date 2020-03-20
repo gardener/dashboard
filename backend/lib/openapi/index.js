8b7df2cfadca314fbbdb86b30ec8a37a7c62e032
Lukas Gross <35373687+grolu@users.noreply.github.com>
2020-03-20 13:27:57 +0100

    Update backend/lib/openapi/index.js
    Co-Authored-By: Holger Koser <holger.koser@sap.com>

--- backend/lib/openapi/index.js
+++ backend/lib/openapi/index.js
@@ -25,32 +25,39 @@ const _ = require('lodash')
 
 const router = module.exports = express.Router()
 
-router.route('/shoot')
+router.route('/')
   .get(async (req, res, next) => {
     try {
       const user = req.user
-      const spec = await getShootSchema(user)
-      res.send({ spec })
+      const schemaDefinitions = await getSchemaDefinitions(user)
+      res.send(schemaDefinitions)
     } catch (err) {
       next(err)
     }
   })
 
-let shootSchema // Cache, TODO: Need to update cache when apiserver gets updated
+const schemaDefinitions = {} // Cache, TODO: Need to update cache when apiserver gets updated
 
-async function getShootSchema (user) {
-  if (!await canGetOpenAPI(user)) {
-    throw new Forbidden('User is not allowed to fetch the Open API specification')
+async function getSchemaDefinitions (user) {
+  const hasAuthorization = await canGetOpenAPI(user)
+  if (!hasAuthorization) {
+    throw new Forbidden('User is not allowed to read OpenAPI schemas definitions')
   }
 
-  if (shootSchema) {
-    return shootSchema
+  if (_.isEmpty(schemaDefinitions)) {
+    // Do not use client of user as the result gets cached and returned to other users
+    const swaggerApi = await dashboardClient.openapi.get()
+    const dereferencedSwaggerApi  = await SwaggerParser.dereference(swaggerApi)
+
+    const selectedSchemaDefinitions = _
+      .chain(dereferencedSwaggerApi)
+      .get('definitions')
+      .pick([
+        'com.github.gardener.gardener.pkg.apis.core.v1beta1.Shoot'
+      ])
+      .value()
+    _.assign(schemaDefinitions, selectedSchemaDefinitions)
   }
 
-  // Do not use client of user as the result gets cached and returned to other users
-  const res = await dashboardClient.openapi.get()
-  const spec = await SwaggerParser.dereference(res)
-
-  shootSchema = _.get(spec, ['definitions', 'com.github.gardener.gardener.pkg.apis.core.v1beta1.Shoot'], {})
-  return shootSchema
+  return schemaDefinitions
 }
