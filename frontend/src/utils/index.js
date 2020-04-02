14b025ca5a016072bee985431f105123a73c9f7d
Lukas Gross <35373687+grolu@users.noreply.github.com>
2020-04-02 06:42:27 +0200

    Do not accept values other than ^(\d+)Gi$ (#652)
    * Do not accept values other than ^(\d+)Gi$
    
    * Do not test value

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -182,7 +182,14 @@ export function displayName (username) {
 }
 
 export function parseSize (value) {
-  return parseInt(replace(value, /(^.+\D)(\d+)(\D.+$)/i, '$2'))
+  const sizeRegex = /^(\d+)Gi$/
+  const result = sizeRegex.exec(value)
+  if (result) {
+    const [, sizeValue] = result
+    return sizeValue
+  }
+  console.error(`Could not parse size ${value} as it does not match regex ^(\\d+)Gi$`)
+  return 0
 }
 
 export function isEmail (value) {
