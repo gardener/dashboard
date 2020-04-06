20448fefb53985ec595113953578d41ebd381193
Holger Koser <holger.koser@sap.com>
2020-04-06 22:55:38 +0200

    Allow account avatar color configuration

--- frontend/src/components/AccountAvatar.vue
+++ frontend/src/components/AccountAvatar.vue
@@ -19,19 +19,25 @@ limitations under the License.
     <v-avatar :size="size">
       <img :src="avatarUrl"/>
     </v-avatar>
-    <a v-if="mailTo && isEmail" :href="`mailto:${accountName}`" class="pl-2 cyan--text text--darken-2">{{accountName}}</a>
+    <a v-if="mailTo && isEmail" :href="`mailto:${accountName}`" class="pl-2" :class="textColor">{{accountName}}</a>
     <span v-else class="pl-2">{{accountName || '-unknown-'}}</span>
   </div>
 </template>
 
 <script>
 import { gravatarUrlGeneric, isEmail } from '@/utils'
+import split from 'lodash/split'
+import map from 'lodash/map'
 
 export default {
   props: {
     accountName: {
       type: String,
       default: ''
+    },
+    color: {
+      type: String,
+      default: 'cyan darken-2'
     },
     mailTo: {
       type: Boolean,
@@ -43,6 +49,10 @@ export default {
     }
   },
   computed: {
+    textColor () {
+      const iteratee = value => /^(darken|lighten|accent)-\d$/.test(value) ? 'text--' + value : value + '--text'
+      return map(split(this.color, ' '), iteratee)
+    },
     avatarUrl () {
       return gravatarUrlGeneric(this.accountName, this.size * 2)
     },
