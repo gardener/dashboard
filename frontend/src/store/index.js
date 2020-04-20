7cd4deea4e7a76c28784a3e18cb6086d1594fa42
Lukas Gross <35373687+grolu@users.noreply.github.com>
2020-04-20 09:53:45 +0200

    Filter floating pools that start or end with wildcard (#667)

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -53,6 +53,8 @@ import members from './modules/members'
 import infrastructureSecrets from './modules/infrastructureSecrets'
 import journals from './modules/journals'
 import semver from 'semver'
+import startsWith from 'lodash/startsWith'
+import endsWith from 'lodash/endsWith'
 
 Vue.use(Vuex)
 
@@ -388,7 +390,11 @@ const getters = {
     return (cloudProfileName, region) => {
       const cloudProfile = getters.cloudProfileByName(cloudProfileName)
       const floatingPools = get(cloudProfile, 'data.providerConfig.constraints.floatingPools')
-      const availableFloatingPools = filter(floatingPools, matchesPropertyOrEmpty('region', region))
+      let availableFloatingPools = filter(floatingPools, matchesPropertyOrEmpty('region', region))
+      availableFloatingPools = filter(floatingPools, fp => {
+        const wildcard = startsWith(fp.name, '*') || endsWith(fp.name, '*')
+        return !wildcard // TODO introduce wildcard component to make wildcard fps (and potential other values) configurable on UI
+      })
       return uniq(map(availableFloatingPools, 'name'))
     }
   },
