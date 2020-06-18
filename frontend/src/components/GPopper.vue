d6003a7030a0700b6ee2bcf4673240f70720b9f0
Lukas Gross <lukas.gross@sap.com>
2020-06-18 18:09:05 +0200

    limit height of poppers according to viewport size

--- frontend/src/components/GPopper.vue
+++ frontend/src/components/GPopper.vue
@@ -144,6 +144,8 @@ export default {
     -webkit-box-shadow: 0 5px 5px -3px rgba(0,0,0,0.2), 0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12);
     box-shadow: 0 5px 5px -3px rgba(0,0,0,0.2), 0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12);
     max-width: 1000px;
+    max-height: 85vh;
+    overflow-y: scroll;
   }
 
   ::v-deep .v-toolbar__content {
