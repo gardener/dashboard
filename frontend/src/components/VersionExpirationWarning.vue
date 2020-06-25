b8f226f783d54693c6128ee21a85f96488e37abd
Lukas Gross <lukas.gross@sap.com>
2020-06-25 16:45:38 +0200

    show version warning as popper

--- frontend/src/components/VersionExpirationWarning.vue
+++ frontend/src/components/VersionExpirationWarning.vue
@@ -15,25 +15,40 @@ limitations under the License.
 -->
 
 <template>
-  <v-tooltip top v-if="k8sExpirationDate || machineImageExpirationDates.length">
-    <template v-slot:activator="{ on }">
-      <v-icon v-on="on" color="warning">mdi-update</v-icon>
+  <g-popper
+    v-if="k8sExpirationDate || expiredWorkerGroups.length"
+    :title="`Update Warnings for ${shootName}`"
+    toolbarColor="warning"
+    :popperKey="`version_warning${shootName}`"
+  >
+    <template v-slot:popperRef>
+      <div>
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-icon v-on="on" color="warning" class="cursor-pointer">mdi-update</v-icon>
+          </template>
+          <span>Version update warning</span>
+        </v-tooltip>
+      </div>
     </template>
-    <div v-if="k8sExpirationDate">Kubernetes version of this cluster is about to expire. The version update will be enforced <span class="font-weight-bold"><time-string :date-time="k8sExpirationDate"></time-string></span></div>
-    <div v-for="({expirationDate, version, name, workerName, key}) in expiredWorkerGroups" :key="key">
-      Machine image
-      <span class="font-weight-bold">{{name}} | Version: {{version}}</span>
-      of worker group
-      <span class="font-weight-bold">{{workerName}}</span>
-      is about to expire. The version update will be enforced
-      <span class="font-weight-bold"><time-string :date-time="expirationDate"></time-string></span>
-    </div>
-  </v-tooltip>
+    <ul class="update-warning-box">
+      <li v-if="k8sExpirationDate">Kubernetes version of this cluster is about to expire. The version update will be enforced <span class="font-weight-bold"><time-string :date-time="k8sExpirationDate"></time-string></span></li>
+      <li v-for="({expirationDate, version, name, workerName, key}) in expiredWorkerGroups" :key="key">
+        Machine image
+        <span class="font-weight-bold">{{name}} | Version: {{version}}</span>
+        of worker group
+        <span class="font-weight-bold">{{workerName}}</span>
+        is about to expire. The version update will be enforced
+        <span class="font-weight-bold"><time-string :date-time="expirationDate"></time-string></span>
+      </li>
+    </ul>
+  </g-popper>
 </template>
 
 <script>
 
 import TimeString from '@/components/TimeString'
+import GPopper from '@/components/GPopper'
 import find from 'lodash/find'
 import forEach from 'lodash/forEach'
 import get from 'lodash/get'
@@ -43,11 +58,18 @@ import { shootItem } from '@/mixins/shootItem'
 export default {
   name: 'VerisonUpdateWarning',
   components: {
-    TimeString
+    TimeString,
+    GPopper
   },
   props: {
     shootItem: {
       type: Object
+    },
+    onlyK8sWarnings: {
+      type: Boolean
+    },
+    onlyMachineImageWarnings: {
+      type: Boolean
     }
   },
   mixins: [shootItem],
@@ -57,6 +79,9 @@ export default {
       'machineImagesByCloudProfileName'
     ]),
     k8sExpirationDate () {
+      if (this.onlyMachineImageWarnings) {
+        return undefined
+      }
       const allVersions = this.kubernetesVersions(this.shootCloudProfileName)
       const version = find(allVersions, { version: this.shootK8sVersion })
       if (version) {
@@ -65,19 +90,21 @@ export default {
       return undefined
     },
     expiredWorkerGroups () {
+      if (this.onlyK8sWarnings) {
+        return []
+      }
       const expiredWorkerGroups = []
       const allMachineImages = this.machineImagesByCloudProfileName(this.shootCloudProfileName)
       forEach(this.shootItem.spec.provider.workers, worker => {
         const workerImage = get(worker, 'machine.image')
         const workerImageDetails = find(allMachineImages, workerImage)
         if (workerImageDetails.expirationDate) {
-           expiredWorkerGroups.push({
-             ...workerImageDetails,
-             workerName: worker.name
-             })
+          expiredWorkerGroups.push({
+            ...workerImageDetails,
+            workerName: worker.name
+          })
         }
       })
-      console.log(expiredWorkerGroups)
       return expiredWorkerGroups
     }
   }
@@ -85,5 +112,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+  .cursor-pointer {
+      cursor: pointer;
+  }
+
+  .update-warning-box {
+    max-width: 800px;
+    text-align: left;
+  }
 
 </style>
