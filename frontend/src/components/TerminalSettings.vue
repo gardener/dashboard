5114555b18d6850b0b2ffcc51626dadb0b9a38d1
Peter Sutter <peter.sutter@sap.com>
2020-09-15 20:52:35 +0200

    Apply suggestions from code review
    Co-authored-by: Holger Koser <holger.koser@sap.com>

--- frontend/src/components/TerminalSettings.vue
+++ frontend/src/components/TerminalSettings.vue
@@ -192,7 +192,9 @@ export default {
       return this.selectedRunOnShootWorker ? 'black--text' : 'grey--text'
     },
     selectedConfig () {
-      const node = this.selectedNode === this.autoSelectNodeItem.data.kubernetesHostname ? undefined : this.selectedNode
+      const node = this.selectedNode === this.autoSelectNodeItem.data.kubernetesHostname 
+        ? undefined 
+        : this.selectedNode
       const selectedConfig = {
         container: {
           image: this.selectedContainerImage
@@ -214,7 +216,9 @@ export default {
     initialize ({ container = {}, defaultNode, currentNode, privilegedMode, nodes = [] }) {
       this.selectedContainerImage = container.image
       if (!defaultNode) {
-        defaultNode = this.isAdmin ? get(head(nodes), 'data.kubernetesHostname') : this.autoSelectNodeItem.data.kubernetesHostname
+        defaultNode = this.isAdmin 
+          ? get(head(nodes), 'data.kubernetesHostname') 
+          : this.autoSelectNodeItem.data.kubernetesHostname
       }
       this.selectedNode = defaultNode
       this.shootNodes = [this.autoSelectNodeItem, ...nodes]
