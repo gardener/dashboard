1538be54ab96c76b32f8c5ad556a195554d24056
Matthias Sohn <matthias.sohn@sap.com>
2020-11-03 11:35:10 +0100

    Declare license and copyright information using REUSE (#811)
    We now use https://reuse.software/ to declare license and copyright
    information in a machine readable format.
    
    The copyright statement for SAP open source projects was updated
    to give credit to project contributors not employed by SAP.
    
    Cached dependencies under .yarn/ will be adapted in a follow-up change.

--- frontend/src/components/icons/IconBase.vue
+++ frontend/src/components/icons/IconBase.vue
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
 <template>
   <svg xmlns="http://www.w3.org/2000/svg"
     :width="width"
