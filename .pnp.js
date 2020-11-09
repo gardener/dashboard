8e113d44d8c3bd44f270b85d2b2546faabc73ad2
Holger Koser <holger.koser@sap.com>
2020-11-09 08:46:23 +0100

    Specify supported versions of `node` and `yarn` (#876)
    * remove spdx header from generates files
    
    * fix yamlrc lint error
    
    * require node version 14 and yarn version 2 via engines
    
    * Only >= latest stable versions

--- .pnp.js
+++ .pnp.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-//
-// SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors
-//
-// SPDX-License-Identifier: Apache-2.0
-
 /* eslint-disable */
 
 try {
