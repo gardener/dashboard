thedemodev/gardener-dashboard

Added support to read controllerregistration resources Show dropdown for registered network extensions Show network etension list (for operators)

backend/test/cache.spec.js
@@ -58,11 +58,19 @@ describe('cache', function () {
   }
   Project.scope = 'Cluster'
 
+  class ControllerRegistration {
+    syncList (store) {
+      return replace(store, [a, c])
+    }
+  }
+  ControllerRegistration.scope = 'Cluster'
+
   const gardenerCore = {
     cloudprofiles: new CloudProfile(),
     quotas: new Quota(),
     seeds: new Seed(),
-    projects: new Project()
+    projects: new Project(),
+    controllerregistrations: new ControllerRegistration()
   }
 
   const testClient = {
@@ -103,6 +111,12 @@ describe('cache', function () {
     expect(stub).to.be.calledOnce
   })
 
+  it('should dispatch "getControllerRegistrations" to internal cache', function () {
+    const stub = sandbox.stub(internalCache, 'getControllerRegistrations').returns(list)
+    expect(cache.getControllerRegistrations()).to.equal(list)
+    expect(stub).to.be.calledOnce
+  })
+
   describe('Cache', function () {
     const Cache = internalCache.constructor
     let cache
@@ -116,12 +130,14 @@ describe('cache', function () {
       let syncQuotasSpy
       let syncSeedsSpy
       let syncProjectsSpy
+      let syncControllerregistrationsSpy
 
       beforeEach(function () {
         syncCloudprofilesSpy = sandbox.spy(gardenerCore.cloudprofiles, 'syncList')
         syncQuotasSpy = sandbox.spy(gardenerCore.quotas, 'syncListAllNamespaces')
         syncSeedsSpy = sandbox.spy(gardenerCore.seeds, 'syncList')
         syncProjectsSpy = sandbox.spy(gardenerCore.projects, 'syncList')
+        syncControllerregistrationsSpy = sandbox.spy(gardenerCore.controllerregistrations, 'syncList')
       })
 
       it('should syncronize the cache', async function () {
@@ -131,6 +147,7 @@ describe('cache', function () {
         expect(syncQuotasSpy).to.be.calledOnce
         expect(syncSeedsSpy).to.be.calledOnce
         expect(syncProjectsSpy).to.be.calledOnce
+        expect(syncControllerregistrationsSpy).to.be.calledOnce
         expect(cache.synchronizationPromise).to.be.instanceof(Promise)
         expect(orderBy(cache.getCloudProfiles(), 'metadata.uid')).to.eql([a, c])
         expect(orderBy(cache.getQuotas(), 'metadata.uid')).to.eql([a, b, c])

gardener/dashboard

backend/test/cache.spec.js
@@ -55,11 +55,19 @@ describe('cache', function () {
   }
   Project.scope = 'Cluster'
 
+  class ControllerRegistration {
+    syncList (store) {
+      return replace(store, [a, c])
+    }
+  }
+  ControllerRegistration.scope = 'Cluster'
+
   const gardenerCore = {
     cloudprofiles: new CloudProfile(),
     quotas: new Quota(),
     seeds: new Seed(),
-    projects: new Project()
+    projects: new Project(),
+    controllerregistrations: new ControllerRegistration()
   }
 
   const testClient = {
@@ -97,6 +105,12 @@ describe('cache', function () {
     expect(stub).toBeCalledTimes(1)
   })
 
+  it('should dispatch "getControllerRegistrations" to internal cache', function () {
+    const stub = jest.spyOn(internalCache, 'getControllerRegistrations').mockReturnValue(list)
+    expect(cache.getControllerRegistrations()).toEqual(list)
+    expect(stub).toBeCalledTimes(1)
+  })
+
   describe('Cache', function () {
     const Cache = internalCache.constructor
     let cache
@@ -110,12 +124,14 @@ describe('cache', function () {
       let syncQuotasSpy
       let syncSeedsSpy
       let syncProjectsSpy
+      let syncControllerregistrationsSpy
 
       beforeEach(function () {
         syncCloudprofilesSpy = jest.spyOn(gardenerCore.cloudprofiles, 'syncList')
         syncQuotasSpy = jest.spyOn(gardenerCore.quotas, 'syncListAllNamespaces')
         syncSeedsSpy = jest.spyOn(gardenerCore.seeds, 'syncList')
         syncProjectsSpy = jest.spyOn(gardenerCore.projects, 'syncList')
+        syncControllerregistrationsSpy = jest.spyOn(gardenerCore.controllerregistrations, 'syncList')
       })
 
       it('should syncronize the cache', async function () {
@@ -125,11 +141,13 @@ describe('cache', function () {
         expect(syncQuotasSpy).toBeCalledTimes(1)
         expect(syncSeedsSpy).toBeCalledTimes(1)
         expect(syncProjectsSpy).toBeCalledTimes(1)
+        expect(syncControllerregistrationsSpy).toBeCalledTimes(1)
         expect(cache.synchronizationPromise).toBeInstanceOf(Promise)
         expect(orderBy(cache.getCloudProfiles(), 'metadata.uid')).toEqual([a, c])
         expect(orderBy(cache.getQuotas(), 'metadata.uid')).toEqual([a, b, c])
         expect(orderBy(cache.getSeeds(), 'metadata.uid')).toEqual([a, b])
         expect(orderBy(cache.getProjects(), 'metadata.uid')).toEqual([a, b, c, d])
+        expect(orderBy(cache.getControllerRegistrations(), 'metadata.uid')).toEqual([a, c])
       })
     })
 