1b3790a34069add67e05c6f860b753445af52b3c
Lukas Gross <lukas.gross@sap.com>
2020-12-21 16:42:36 +0100

    Fixed Control Plane Migration Indication

--- frontend/src/components/ShootSeedName.vue
+++ frontend/src/components/ShootSeedName.vue
@@ -6,27 +6,17 @@ SPDX-License-Identifier: Apache-2.0
 
 <template>
   <v-row align="center" class="ma-0">
-    <v-tooltip top :disabled="!isControlPlaneMigrating">
+    <v-tooltip top :disabled="!isShootLastOperationTypeControlPlaneMigrating">
       <template v-slot:activator="{ on }">
         <div v-on="on">
-          <template v-if="isControlPlaneMigrating">
-            <v-progress-circular indeterminate size=12 width=2 color="cyan darken-2" class="mr-1"></v-progress-circular>
-            <router-link v-if="canLinkToSeed" class="cyan--text text--darken-2" :to="{ name: 'ShootItem', params: { name: shootStatusSeedName, namespace:'garden' } }">
-              <span>{{shootStatusSeedName}}</span>
-            </router-link>
-            <span v-else>{{shootSeedName}}</span>
-            <v-icon small class="mx-1">mdi-arrow-right</v-icon>
-          </template>
+          <v-progress-circular v-if="isShootLastOperationTypeControlPlaneMigrating" indeterminate size=12 width=2 color="cyan darken-2" class="mr-1"></v-progress-circular>
           <router-link v-if="canLinkToSeed" class="cyan--text text--darken-2" :to="{ name: 'ShootItem', params: { name: shootSeedName, namespace:'garden' } }">
             <span>{{shootSeedName}}</span>
           </router-link>
           <span v-else>{{shootSeedName}}</span>
         </div>
       </template>
-      <span>Control Plane is migrating </span>
-      <span class="font-weight-bold">{{shootStatusSeedName}}</span>
-      <v-icon size=14 color="white">mdi-arrow-right</v-icon>
-      <span class="font-weight-bold">{{shootSeedName}}</span>
+      <span>{{shootLastOperationTypeControlPlaneMigrationMessage}}</span>
     </v-tooltip>
   </v-row>
 </template>
