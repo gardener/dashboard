4b36ab02f13b09ec3f761623c9c2e0d17c357cb3
Lukas Gross <35373687+grolu@users.noreply.github.com>
2021-02-22 13:54:52 +0100

    Apply suggestions from code review
    Co-authored-by: Holger Koser <holger.koser@sap.com>
    Co-authored-by: Peter Sutter <peter.sutter@sap.com>

--- frontend/src/components/dialogs/InfoDialog.vue
+++ frontend/src/components/dialogs/InfoDialog.vue
@@ -12,7 +12,7 @@ SPDX-License-Identifier: Apache-2.0
     maxWidth="600"
     @dialog-closed="onDialogClosed()"
     >
-    <template v-slot:caption>About Gardener Dashboard</template>
+    <template v-slot:caption>About</template>
     <template v-slot:message>
       <div class="d-flex flex-row align-center mt-3">
         <img src="../../assets/logo.svg" class="logo mr-3">
@@ -23,10 +23,10 @@ SPDX-License-Identifier: Apache-2.0
       <v-divider class="my-3"></v-divider>
       <div class="grey--text text--darken-1">
         <div class="font-weight-bold">Version Information</div>
-        <div v-if="!!dashboardVersion">Dashboard version {{dashboardVersion}}</div>
-        <div v-if="!!gardenerVersion">API version {{gardenerVersion}}</div>
+        <div v-if="!!dashboardVersion">Dashboard {{dashboardVersion}}</div>
+        <div v-if="!!gardenerVersion">API {{gardenerVersion}}</div>
         <v-divider v-if="gardenerExtensionsList.length" class="my-3"></v-divider>
-        <div v-if="gardenerExtensionsList.length" class="font-weight-bold">{{gardenerExtensionsList.length}} deployed Extensions</div>
+        <div v-if="gardenerExtensionsList.length" class="font-weight-bold">Extensions ({{gardenerExtensionsList.length}} deployed)</div>
         <div
         v-for="extension in gardenerExtensionsList"
         :key="extension.id"
