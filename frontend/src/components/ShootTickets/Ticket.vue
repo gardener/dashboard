c877f256b84dfd19a11b50fbd7af02fb8874ccb4
Lukas Gross <35373687+grolu@users.noreply.github.com>
2021-05-19 14:29:28 +0200

    Fixed Ticket ADD COMMENT styling (#1020)

--- frontend/src/components/ShootTickets/Ticket.vue
+++ frontend/src/components/ShootTickets/Ticket.vue
@@ -25,7 +25,7 @@ SPDX-License-Identifier: Apache-2.0
     </v-list>
     <v-card-actions v-if="!!gitHubRepoUrl">
       <v-spacer></v-spacer>
-      <v-btn text class="action-button primary--text" :href="addCommentLink" target="_blank" title="Add Comment">
+      <v-btn text color="primary" :href="addCommentLink" target="_blank" title="Add Comment">
         Add Comment
         <v-icon color="anchor" class="link-icon pl-2">mdi-open-in-new</v-icon>
       </v-btn>
