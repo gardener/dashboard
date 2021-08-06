7b8042c71aa1adc5580d925d5eec04734cddfd33
Lukas Gross <lukas.gross@sap.com>
2021-08-06 11:43:58 +0200

    Fix controllerregistrations permission issue

--- backend/lib/services/controllerregistrations.js
+++ backend/lib/services/controllerregistrations.js
@@ -6,23 +6,36 @@
 
 'use strict'
 
-const { Forbidden } = require('http-errors')
 const { getControllerRegistrations } = require('../cache')
 const authorization = require('./authorization')
 const _ = require('lodash')
 
 exports.listExtensions = async function ({ user }) {
-  const allowed = await authorization.canListControllerRegistrations(user)
-  if (!allowed) {
-    throw new Forbidden('You are not allowed to list controllerregistrations')
-  }
-
   const controllerregistrations = getControllerRegistrations()
-  return _.map(controllerregistrations, ({ metadata, spec }) => {
+  // required resoure kinds are essential for the frontend and need to be returned even if the user has not the right to read controllerregistrations
+  const requiredResourceKinds = ['Network', 'DNSProvider']
+  const stripNotRequiredInformation = ({ metadata, spec }) => {
+    const resources = _.filter(spec.resources, resource => resource && _.includes(requiredResourceKinds, resource.kind))
     return {
       name: metadata.name,
-      version: _.get(spec, 'deployment.providerConfig.values.image.tag'),
-      resources: _.get(spec, 'resources')
+      resources
     }
-  })
+  }
+
+  const allowed = await authorization.canListControllerRegistrations(user)
+  if (!allowed) {
+    return _
+      .chain(controllerregistrations)
+      .map(stripNotRequiredInformation)
+      .filter(({ resources }) => !_.isEmpty(resources))
+      .value()
+  } else {
+    return _.map(controllerregistrations, ({ metadata, spec }) => {
+      return {
+        name: metadata.name,
+        version: _.get(spec, 'deployment.providerConfig.values.image.tag'),
+        resources: _.get(spec, 'resources')
+      }
+    })
+  }
 }
