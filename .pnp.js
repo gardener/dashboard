45b721b6e9680ddf503f2813a338197bd2a5c64c
Holger Koser <holger.koser@sap.com>
2022-05-06 22:43:41 +0200

    Updated caniuse-lite to latest version

--- .pnp.js
+++ .pnp.js
@@ -5700,7 +5700,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["autoprefixer", "npm:9.8.6"],
             ["browserslist", "npm:4.16.4"],
-            ["caniuse-lite", "npm:1.0.30001258"],
+            ["caniuse-lite", "npm:1.0.30001338"],
             ["colorette", "npm:1.2.2"],
             ["normalize-range", "npm:0.1.2"],
             ["num2fraction", "npm:1.2.2"],
@@ -6537,7 +6537,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/browserslist-npm-4.16.4-9ca361ea82-00071f5169.zip/node_modules/browserslist/",
           "packageDependencies": [
             ["browserslist", "npm:4.16.4"],
-            ["caniuse-lite", "npm:1.0.30001258"],
+            ["caniuse-lite", "npm:1.0.30001338"],
             ["colorette", "npm:1.2.2"],
             ["electron-to-chromium", "npm:1.3.717"],
             ["escalade", "npm:3.1.1"],
@@ -6883,7 +6883,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["caniuse-api", "npm:3.0.0"],
             ["browserslist", "npm:4.16.4"],
-            ["caniuse-lite", "npm:1.0.30001258"],
+            ["caniuse-lite", "npm:1.0.30001338"],
             ["lodash.memoize", "npm:4.1.2"],
             ["lodash.uniq", "npm:4.5.0"]
           ],
@@ -6891,10 +6891,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["caniuse-lite", [
-        ["npm:1.0.30001258", {
-          "packageLocation": "./.yarn/cache/caniuse-lite-npm-1.0.30001258-9014d665ba-ef97b5eb31.zip/node_modules/caniuse-lite/",
+        ["npm:1.0.30001338", {
+          "packageLocation": "./.yarn/cache/caniuse-lite-npm-1.0.30001338-0f5d19b25f-89a9072725.zip/node_modules/caniuse-lite/",
           "packageDependencies": [
-            ["caniuse-lite", "npm:1.0.30001258"]
+            ["caniuse-lite", "npm:1.0.30001338"]
           ],
           "linkType": "HARD",
         }]
