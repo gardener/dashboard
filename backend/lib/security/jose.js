1e5afc8c21d79f84306c6b2cd4579c9bccb59df6
Holger Koser <holger.koser@sap.com>
2022-07-06 22:09:27 +0200

    allow jti and exp claims in payload

--- backend/lib/security/jose.js
+++ backend/lib/security/jose.js
@@ -53,7 +53,7 @@ module.exports = sessionSecret => {
   return {
     encodeState,
     decodeState,
-    sign (payload, secretOrPrivateKey, options) {
+    sign (payload, secretOrPrivateKey, { expiresIn = '1d', jwtid = uuid.v1(), ...options } = {}) {
       if (isPlainObject(secretOrPrivateKey)) {
         options = secretOrPrivateKey
         secretOrPrivateKey = undefined
@@ -61,8 +61,13 @@ module.exports = sessionSecret => {
       if (!secretOrPrivateKey) {
         secretOrPrivateKey = sessionSecret
       }
-      const { expiresIn = '1d', jwtid = uuid.v1(), ...rest } = options || {}
-      return jwtSign(payload, secretOrPrivateKey, { expiresIn, jwtid, ...rest })
+      if (!payload.exp) {
+        options.expiresIn = expiresIn
+      }
+      if (!payload.jti) {
+        options.jwtid = jwtid
+      }
+      return jwtSign(payload, secretOrPrivateKey, options)
     },
     verify (token, options) {
       return jwtVerify(token, sessionSecret, options)
