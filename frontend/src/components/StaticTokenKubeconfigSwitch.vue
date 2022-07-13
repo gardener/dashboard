4147460d435209c99c97518afdfac67e6378ea6b
Lukas Gross <lukas.gross@sap.com>
2022-07-13 11:07:49 +0200

    PR Feedback #2

--- frontend/src/components/StaticTokenKubeconfigSwitch.vue
+++ frontend/src/components/StaticTokenKubeconfigSwitch.vue
@@ -10,13 +10,12 @@ SPDX-License-Identifier: Apache-2.0
       v-model="enableStaticTokenKubeconfig"
       label="Enable static token kubeconfig"
       color="primary"
-      @change="onChange"
       hide-details
     ></v-checkbox>
-    <span class="text-caption">It is <span class="font-weight-bold">not</span> the recommended method to access the shoot cluster as the static token <code>kubeconfig</code> has some security flaws associated with it.</span>
-    <p>
+    <div class="text-caption mt-1">It is <strong>not</strong> the recommended method to access the shoot cluster as the static token <code>kubeconfig</code> has some security flaws associated with it.</div>
+    <div>
       <external-link class="text-caption" url="https://github.com/gardener/gardener/blob/master/docs/usage/shoot_access.md">More information</external-link>
-    </p>
+    </div>
   </div>
 </template>
 <script>
@@ -30,23 +29,20 @@ export default {
   },
   props: {
     value: {
-      type: Boolean
+      type: Boolean,
+      default: false
     }
   },
-  data () {
-    return {
-      enableStaticTokenKubeconfig: undefined
-    }
-  },
-  methods: {
-    onChange (value) {
-      this.$emit('input', value)
-    }
-  },
-  watch: {
-    value: function (value) {
-      this.enableStaticTokenKubeconfig = value
+  computed: {
+    enableStaticTokenKubeconfig: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        this.$emit('input', val)
+      }
     }
   }
+
 }
 </script>
