825829f22b3ee59042648b7ee3906f388b441b44
Lukas Gross <lukas.gross@sap.com>
2022-07-21 13:55:24 +0200

    Adapted tests

--- backend/test/services.projects.spec.js
+++ backend/test/services.projects.spec.js
@@ -84,7 +84,7 @@ describe('services', function () {
     describe('#list', function () {
       it('should return all projects if user is admin, including not ready projects', async function () {
         const userProjects = await projects.list({ user: createUser('admin@bar.com') })
-        expect(userProjects).toHaveLength(3)
+        expect(userProjects).toHaveLength(2)
       })
 
       it('should return project for user member', async function () {
