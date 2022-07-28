62c5a516d826d15b0e76fc85c6947db344b3925a
Peter Sutter <peter.sutter@sap.com>
2022-07-28 18:16:49 +0200

    add backend test

--- backend/test/acceptance/config.spec.js
+++ backend/test/acceptance/config.spec.js
@@ -23,15 +23,40 @@ describe('config', function () {
     mockRequest.mockReset()
   })
 
-  it('should return the frontend configuration', async function () {
-    mockRequest.mockResolvedValueOnce({ data: { 'cluster-identity': 'test-id' } })
-
+  it('should return the public frontend configuration', async function () {
     const res = await agent
       .get('/config.json')
       .expect('content-type', /json/)
       .expect(200)
 
+    expect(mockRequest.mock.calls).toMatchSnapshot()
+
+    expect(res.body).toMatchSnapshot()
+  })
+
+  it('should return the frontend configuration', async function () {
+    mockRequest.mockResolvedValueOnce({ data: { 'cluster-identity': 'test-id' } })
+
+    const user = fixtures.auth.createUser({ id: 'john.doe@example.org' })
+
+    const res = await agent
+      .get('/api/config')
+      .set('cookie', await user.cookie)
+      .expect('content-type', /json/)
+      .expect(200)
+
     expect(mockRequest).toBeCalledTimes(1)
+    expect(mockRequest.mock.calls).toMatchSnapshot()
+
+    expect(res.body).toMatchSnapshot()
+  })
+
+  it('should reject unauthenticated requests', async function () {
+    const res = await agent
+      .get('/api/config')
+      .expect('content-type', /json/)
+      .expect(401)
+
     expect(mockRequest.mock.calls).toMatchSnapshot()
 
     expect(res.body).toMatchSnapshot()
