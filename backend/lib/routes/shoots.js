96ca6590dfeabcb38296d6f7041b685991f56cc9
Lukas Gross <35373687+grolu@users.noreply.github.com>
2022-08-16 14:01:05 +0200

    Implemented Disable Gardener provided static admin kubeconfig (#1249)
    * Implemented Disable Gardener provided static admin kubeconfig
    
    * PR Feedback
    
    * PR Feedback #1
    
    * PR Feedback #2
    
    * PR Feedback
    
    * Show kubeconfig tile only if kubeconfig available OR user can patch shoots
    
    * Update frontend/src/components/StaticTokenKubeconfigSwitch.vue
    
    Co-authored-by: Peter Sutter <peter.sutter@sap.com>
    
    Co-authored-by: Peter Sutter <peter.sutter@sap.com>

--- backend/lib/routes/shoots.js
+++ backend/lib/routes/shoots.js
@@ -67,6 +67,19 @@ router.route('/:name')
     }
   })
 
+router.route('/:name/spec/kubernetes/enableStaticTokenKubeconfig')
+  .put(async (req, res, next) => {
+    try {
+      const user = req.user
+      const namespace = req.params.namespace
+      const name = req.params.name
+      const body = req.body
+      res.send(await shoots.replaceEnableStaticTokenKubeconfig({ user, namespace, name, body }))
+    } catch (err) {
+      next(err)
+    }
+  })
+
 router.route('/:name/spec/kubernetes/version')
   .put(async (req, res, next) => {
     try {
--- frontend/src/mixins/shootItem.js
+++ frontend/src/mixins/shootItem.js
@@ -109,6 +109,9 @@ export const shootItem = {
     shootK8sVersion () {
       return get(this.shootSpec, 'kubernetes.version')
     },
+    shootEnableStaticTokenKubeconfig () {
+      return get(this.shootSpec, 'kubernetes.enableStaticTokenKubeconfig', true)
+    },
     shootCloudProfileName () {
       return this.shootSpec.cloudProfileName
     },
