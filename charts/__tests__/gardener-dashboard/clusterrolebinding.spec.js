432d59253ef3d01b49f80c655b9a347ad979a833
Peter Sutter <peter.sutter@sap.com>
2022-08-29 17:19:54 +0200

    fix clusterrolebinding chart test

--- charts/__tests__/gardener-dashboard/clusterrolebinding.spec.js
+++ charts/__tests__/gardener-dashboard/clusterrolebinding.spec.js
@@ -9,7 +9,7 @@
 const { basename } = require('path')
 const { helm } = fixtures
 
-const chart = basename(__dirname) + '/charts/runtime' // TODO discuss
+const chart = basename(__dirname) + '/charts/application' // TODO discuss
 const renderTemplates = (templates, values) => helm.renderChartTemplates(chart, templates, values)
 
 describe('gardener-dashboard', function () {
@@ -18,7 +18,7 @@ describe('gardener-dashboard', function () {
 
     beforeEach(() => {
       templates = [
-        'rbac'
+        'clusterrolebinding'
       ]
     })
 
@@ -28,18 +28,6 @@ describe('gardener-dashboard', function () {
       expect(documents).toHaveLength(1)
       const [clusterRoleBinding] = documents
       expect(clusterRoleBinding).toMatchSnapshot()
-    })
-
-    it('should not render the template', async function () {
-      const values = {
-        global: {
-          kubeconfig: 'apiVersion: v1'
-        }
-      }
-      const documents = await renderTemplates(templates, values)
-      expect(documents).toHaveLength(1)
-      const [clusterRoleBinding] = documents
-      expect(clusterRoleBinding).toBeFalsy()
     })
   })
 })
