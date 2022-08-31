9863863fe4c18b1eff34848878ea85260b6569b6
Peter Sutter <peter.sutter@sap.com>
2022-08-31 18:07:36 +0200

    add serviceAccountTokenVolumeProjection test

--- charts/__tests__/gardener-dashboard/runtime/deployment.spec.js
+++ charts/__tests__/gardener-dashboard/runtime/deployment.spec.js
@@ -70,5 +70,74 @@ describe('gardener-dashboard', function () {
       const [container] = containers
       expect(container.args).toBeUndefined()
     })
+
+    it('should not project service account token if disabled', async function () {
+      const values = {
+        global: {
+          serviceAccountTokenVolumeProjection: {
+            enabled: false
+          }
+        }
+      }
+      const documents = await renderTemplates(templates, values)
+      expect(documents).toHaveLength(1)
+      const [deployment] = documents
+      const volumes = deployment.spec.template.spec.volumes
+      expect(volumes).toHaveLength(2)
+      expect(volumes).toMatchSnapshot()
+      const containers = deployment.spec.template.spec.containers
+      expect(containers).toHaveLength(1)
+      const [container] = containers
+      expect(container.volumeMounts).toHaveLength(2)
+      expect(container.volumeMounts).toMatchSnapshot()
+    })
+
+    describe('virtual garden', function () {
+      it('should render the template', async function () {
+        const values = {
+          global: {
+            virtualGarden: {
+              enabled: true
+            },
+            serviceAccountTokenVolumeProjection: {
+              enabled: true,
+              expirationSeconds: 3600,
+              audience: 'https://identity.garden.example.org'
+            }
+          }
+        }
+        const documents = await renderTemplates(templates, values)
+        expect(documents).toHaveLength(1)
+        const [deployment] = documents
+        expect(deployment.spec.template.spec.serviceAccountName).toEqual('gardener-dashboard')
+        const volumes = deployment.spec.template.spec.volumes
+        expect(volumes).toHaveLength(3)
+        const [, , serviceAccountTokenVolume] = volumes
+        expect(serviceAccountTokenVolume).toMatchSnapshot()
+        const containers = deployment.spec.template.spec.containers
+        expect(containers).toHaveLength(1)
+        const [container] = containers
+        expect(container.volumeMounts).toHaveLength(3)
+        const [, , serviceAccountTokenVolumeMount] = container.volumeMounts
+        expect(serviceAccountTokenVolumeMount).toMatchSnapshot()
+      })
+
+      it('should use the default service account', async function () {
+        const values = {
+          global: {
+            virtualGarden: {
+              enabled: true
+            },
+            serviceAccountTokenVolumeProjection: {
+              enabled: false
+            }
+          }
+        }
+        const documents = await renderTemplates(templates, values)
+        expect(documents).toHaveLength(1)
+        const [deployment] = documents
+        expect(deployment.spec.template.spec.serviceAccountName).toEqual('default')
+      })
+    })
   })
 })
