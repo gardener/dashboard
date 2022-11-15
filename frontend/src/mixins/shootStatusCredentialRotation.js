34d5dbdcfe16c7db8ae0288ac94be7922192ba54
Lukas Gross <lukas.gross@sap.com>
2022-11-15 17:44:48 +0100

    PR Feedback

--- frontend/src/mixins/shootStatusCredentialRotation.js
+++ frontend/src/mixins/shootStatusCredentialRotation.js
@@ -84,10 +84,10 @@ export const shootStatusCredentialRotation = {
       if (this.phase.type) {
         return this.phase.type
       }
-      if ((this.rotationStatus.lastInitiationTime && !this.rotationStatus.lastCompletionTime) ||
-          (this.rotationStatus.lastInitiationTime &&
-           this.rotationStatus.lastCompletionTime &&
-           this.rotationStatus.lastInitiationTime > this.rotationStatus.lastCompletionTime)) {
+      if (this.rotationStatus.lastInitiationTime &&
+         (!this.rotationStatus.lastCompletionTime ||
+          this.rotationStatus.lastInitiationTime > this.rotationStatus.lastCompletionTime)
+         ) {
         // Show 'Rotating' phase for one step rotations
         return 'Rotating'
       }
