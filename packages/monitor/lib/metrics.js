f2ce46ee7e486f0000dc64e8690ad45f84b6055a
Holger Koser <holger.koser@sap.com>
2023-03-27 16:58:47 +0200

    rename response time metric

--- packages/monitor/lib/metrics.js
+++ packages/monitor/lib/metrics.js
@@ -10,19 +10,23 @@ const promClient = require('prom-client')
 
 const connectionsCount = new promClient.Gauge({
   name: 'tcp_connections_count',
-  labelNames: ['type'],
-  help: 'dashboard currently open connections'
+  help: 'Number of currently open TCP connections',
+  labelNames: ['type']
 })
 const connectionsTotal = new promClient.Counter({
   name: 'tcp_connections_total',
-  labelNames: ['type'],
-  help: 'dashboard total opened connections'
+  help: 'Total number of opened TCP connections',
+  labelNames: ['type']
 })
 const responseTime = new promClient.Histogram({
-  name: 'http_response_duration_seconds',
-  help: 'Time to first byte for requests served in seconds',
+  name: 'http_request_duration_seconds',
+  help: 'HTTP request duration in seconds',
   labelNames: ['method', 'route', 'status_code'],
-  buckets: [0.1, 0.2, 0.3, 0.5, 1.0, 1.5, 2.0, 3.0, 5.0, 10.0, 15.0]
+  buckets: [
+    1e-1, 2e-1, 3e-1, 5e-1,
+    1e+0, 2e+0, 3e+0, 5e+0,
+    1e+1, 2e+1
+  ]
 })
 
 module.exports = {
