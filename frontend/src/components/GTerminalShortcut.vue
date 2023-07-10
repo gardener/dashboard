deb20e1b6f7939f24a58e480d09440e7fc50bbc8
Peter Sutter <peter.sutter@sap.com>
2023-07-10 11:17:05 +0200

    fix shortcuts on details screen. disable multi-sel

--- frontend/src/components/GTerminalShortcut.vue
+++ frontend/src/components/GTerminalShortcut.vue
@@ -5,67 +5,55 @@ SPDX-License-Identifier: Apache-2.0
 -->
 
 <template>
-  <v-item
-    :key="shortcut"
-    :value="shortcut"
-    :disabled="disabled"
-    #default="{ selectedClass, toggle }"
-  >
-    <g-list-item
-      class="cursor-pointer"
-      :class="selectedClass"
-      @click="toggle"
-
-    >
-      <template v-if="!hideIconSlot" #prepend>
-        <slot name="icon"/>
-      </template>
-      <g-list-item-content>
-        {{shortcut.title}}
-        <v-tooltip v-if="isUnverified" location="top" max-width="400px">
-          <template #activator="{ props }">
-            <v-chip
-              v-bind="props"
-              small
-              color="warning"
-              variant="outlined"
-              class="my-0 ml-2"
-            >
-              Unverified
-            </v-chip>
-          </template>
-          This terminal shortcut was created by a member of this project and is not verified by the landscape administrator and therefore could be malicious
-        </v-tooltip>
-        <template v-if="shortcut.description" #description>
-          {{ shortcut.description }}
+  <g-list-item>
+    <template v-if="!hideIconSlot" #prepend>
+      <slot name="icon"/>
+    </template>
+    <g-list-item-content>
+      {{shortcut.title}}
+      <v-tooltip v-if="isUnverified" location="top" max-width="400px">
+        <template #activator="{ props }">
+          <v-chip
+            v-bind="props"
+            small
+            color="warning"
+            variant="outlined"
+            class="my-0 ml-2"
+          >
+            Unverified
+          </v-chip>
         </template>
-      </g-list-item-content>
-      <v-spacer/>
-      <template v-if="!readOnly" #append>
-        <g-action-button
-          icon="mdi-console-line"
-          :disabled="disabled"
-          @click.stop="addTerminalShortcut(shortcut)"
-        >
-          <template #tooltip>
-            <span v-if="!disabled">
-              Create
-              '<span class="font-family-monospace">{{ shortcut.title }}</span>'
-              terminal session
-            </span>
-            <span v-else>
-              Cluster is hibernated. Wake up cluster to open terminal
-            </span>
-          </template>
-        </g-action-button>
-        <g-action-button
-          :icon="visibilityIconShortcut"
-          :tooltip="shortcutVisibilityTitle"
-          @click.stop="expansionPanel = !expansionPanel"
-        />
+        This terminal shortcut was created by a member of this project and is not verified by the landscape administrator and therefore could be malicious
+      </v-tooltip>
+      <template v-if="shortcut.description" #description>
+        {{ shortcut.description }}
       </template>
-    </g-list-item>
-  </v-item>
+    </g-list-item-content>
+    <v-spacer/>
+    <template v-if="!readOnly" #append>
+      <g-action-button
+        icon="mdi-console-line"
+        :disabled="disabled"
+        @click.stop="addTerminalShortcut(shortcut)"
+      >
+        <template #tooltip>
+          <span v-if="!disabled">
+            Create
+            '<span class="font-family-monospace">{{ shortcut.title }}</span>'
+            terminal session
+          </span>
+          <span v-else>
+            Cluster is hibernated. Wake up cluster to open terminal
+          </span>
+        </template>
+      </g-action-button>
+      <g-action-button
+        :icon="visibilityIconShortcut"
+        :tooltip="shortcutVisibilityTitle"
+        @click.stop="expansionPanel = !expansionPanel"
+      />
+    </template>
+  </g-list-item>
   <v-expand-transition>
     <v-card v-if="expansionPanel"
       flat
