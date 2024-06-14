31dd4bfbbbae2e7ec9ffccf358156e1b213b22b6
Holger Koser <holger.koser@sap.com>
2024-06-14 16:44:22 +0200

    fixed vscode test runner for debugging tests

---
+++ scripts/test.cjs
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+//
+// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+const assert = require('assert').strict
+const { spawn } = require('child_process')
+const { findYarnWorkspace } = require('./helper.cjs')
+
+const args =  process.argv.slice(2)
+const workspace = findYarnWorkspace(args[0])
+assert(workspace, 'yarn workspace not found')
+
+const cmd = spawn('yarn', ['workspace', workspace.name, 'run', 'test', ...args], {
+  stdio: 'inherit'
+})
+cmd.once('error', err => process.stderr.write(err.message + '\n'))
+cmd.once('exit', code => process.exit(code))
