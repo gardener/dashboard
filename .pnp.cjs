839944a0060a7e34b89927a7f62fe4172b032e80
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-06-19 12:00:47 +0000

    Update dependency jose to v5.4.1

--- .pnp.cjs
+++ .pnp.cjs
@@ -1518,7 +1518,7 @@ const RAW_RUNTIME_STATE =
           ["helmet", "npm:7.1.0"],\
           ["http-errors", "npm:2.0.0"],\
           ["jest", "virtual:f3f18773c1f2811e8d448670abfc3fed18cdffc11b444f7cbc3548ae5868e74f3c4ee449327c1fc9c24ce0732ee02505411a07539789bec8257188d17bbada1f#npm:29.7.0"],\
-          ["jose", "npm:5.4.0"],\
+          ["jose", "npm:5.4.1"],\
           ["js-yaml", "npm:4.1.0"],\
           ["jsonwebtoken", "npm:9.0.2"],\
           ["lodash", "npm:4.17.21"],\
@@ -8885,17 +8885,17 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["jose", [\
-      ["npm:4.15.5", {\
-        "packageLocation": "./.yarn/cache/jose-npm-4.15.5-15e487a3a1-9f208492f5.zip/node_modules/jose/",\
+      ["npm:4.15.7", {\
+        "packageLocation": "./.yarn/cache/jose-npm-4.15.7-9e745060dd-ba45390927.zip/node_modules/jose/",\
         "packageDependencies": [\
-          ["jose", "npm:4.15.5"]\
+          ["jose", "npm:4.15.7"]\
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:5.4.0", {\
-        "packageLocation": "./.yarn/cache/jose-npm-5.4.0-14d9a81bcc-ef880ee7d9.zip/node_modules/jose/",\
+      ["npm:5.4.1", {\
+        "packageLocation": "./.yarn/cache/jose-npm-5.4.1-82d453ef52-51234a7383.zip/node_modules/jose/",\
         "packageDependencies": [\
-          ["jose", "npm:5.4.0"]\
+          ["jose", "npm:5.4.1"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -10154,7 +10154,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/openid-client-npm-5.6.5-5d3d2c2d1d-4308dcd37a.zip/node_modules/openid-client/",\
         "packageDependencies": [\
           ["openid-client", "npm:5.6.5"],\
-          ["jose", "npm:4.15.5"],\
+          ["jose", "npm:4.15.7"],\
           ["lru-cache", "npm:6.0.0"],\
           ["object-hash", "npm:2.2.0"],\
           ["oidc-token-hash", "npm:5.0.3"]\
