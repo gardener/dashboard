ee21d900351259bd395c47bfd112110b768d3ae7
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-06-19 12:09:59 +0000

    Update dependency @braintree/sanitize-url to v7.0.3

--- .pnp.cjs
+++ .pnp.cjs
@@ -1167,10 +1167,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@braintree/sanitize-url", [\
-      ["npm:7.0.2", {\
-        "packageLocation": "./.yarn/cache/@braintree-sanitize-url-npm-7.0.2-064b98eb4e-11f302fe76.zip/node_modules/@braintree/sanitize-url/",\
+      ["npm:7.0.3", {\
+        "packageLocation": "./.yarn/cache/@braintree-sanitize-url-npm-7.0.3-760c8ddc4a-20c1be13a8.zip/node_modules/@braintree/sanitize-url/",\
         "packageDependencies": [\
-          ["@braintree/sanitize-url", "npm:7.0.2"]\
+          ["@braintree/sanitize-url", "npm:7.0.3"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -1567,7 +1567,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./frontend/",\
         "packageDependencies": [\
           ["@gardener-dashboard/frontend", "workspace:frontend"],\
-          ["@braintree/sanitize-url", "npm:7.0.2"],\
+          ["@braintree/sanitize-url", "npm:7.0.3"],\
           ["@fontsource/roboto", "npm:5.0.13"],\
           ["@kyvg/vue3-notification", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:3.2.1"],\
           ["@mdi/font", "npm:7.4.47"],\
