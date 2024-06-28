62ac0f9d26f1f046796e3f417bcad9955f67f876
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-06-28 12:44:26 +0000

    Update dependency jose to v5.6.2

--- .pnp.cjs
+++ .pnp.cjs
@@ -1510,7 +1510,7 @@ const RAW_RUNTIME_STATE =
           ["helmet", "npm:7.1.0"],\
           ["http-errors", "npm:2.0.0"],\
           ["jest", "virtual:f3f18773c1f2811e8d448670abfc3fed18cdffc11b444f7cbc3548ae5868e74f3c4ee449327c1fc9c24ce0732ee02505411a07539789bec8257188d17bbada1f#npm:29.7.0"],\
-          ["jose", "npm:5.5.0"],\
+          ["jose", "npm:5.6.2"],\
           ["js-yaml", "npm:4.1.0"],\
           ["jsonwebtoken", "npm:9.0.2"],\
           ["lodash", "npm:4.17.21"],\
@@ -8885,10 +8885,10 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:5.5.0", {\
-        "packageLocation": "./.yarn/cache/jose-npm-5.5.0-f42a134c93-e240caa9f7.zip/node_modules/jose/",\
+      ["npm:5.6.2", {\
+        "packageLocation": "./.yarn/cache/jose-npm-5.6.2-6dd6e106fc-86df192545.zip/node_modules/jose/",\
         "packageDependencies": [\
-          ["jose", "npm:5.5.0"]\
+          ["jose", "npm:5.6.2"]\
         ],\
         "linkType": "HARD"\
       }]\
