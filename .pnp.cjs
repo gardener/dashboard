15d4ef9969e84383fde6e5b0505ebeb6ddd327ee
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-07-12 13:47:05 +0000

    Update dependency sass to v1.77.8

--- .pnp.cjs
+++ .pnp.cjs
@@ -1604,7 +1604,7 @@ const RAW_RUNTIME_STATE =
           ["netmask", "npm:2.0.2"],\
           ["pinia", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:2.1.7"],\
           ["rollup-plugin-visualizer", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:5.12.0"],\
-          ["sass", "npm:1.77.7"],\
+          ["sass", "npm:1.77.8"],\
           ["semver", "npm:7.6.2"],\
           ["socket.io-client", "npm:4.7.5"],\
           ["splitpanes", "npm:3.1.5"],\
@@ -11099,10 +11099,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["sass", [\
-      ["npm:1.77.7", {\
-        "packageLocation": "./.yarn/cache/sass-npm-1.77.7-251982e629-6cacbf4b51.zip/node_modules/sass/",\
+      ["npm:1.77.8", {\
+        "packageLocation": "./.yarn/cache/sass-npm-1.77.8-d0ad322666-2bfd627940.zip/node_modules/sass/",\
         "packageDependencies": [\
-          ["sass", "npm:1.77.7"],\
+          ["sass", "npm:1.77.8"],\
           ["chokidar", "npm:3.5.3"],\
           ["immutable", "npm:4.3.4"],\
           ["source-map-js", "npm:1.0.2"]\
@@ -12472,7 +12472,7 @@ const RAW_RUNTIME_STATE =
           ["lightningcss", null],\
           ["postcss", "npm:8.4.39"],\
           ["rollup", "npm:4.13.0"],\
-          ["sass", "npm:1.77.7"],\
+          ["sass", "npm:1.77.8"],\
           ["stylus", null],\
           ["sugarss", null],\
           ["terser", null]\
