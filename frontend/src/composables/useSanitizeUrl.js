671d5f863b9e5d551cd4ad0228d56c7928c957f8
Holger Koser <holger.koser@sap.com>
2024-07-12 16:34:00 +0200

    replace sanitize-url with a simple implementationyarn test

--- frontend/src/composables/useSanitizeUrl.js
+++ frontend/src/composables/useSanitizeUrl.js
@@ -1,12 +1,30 @@
 //
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
 //
 // SPDX-License-Identifier: Apache-2.0
 //
 
 import { unref } from 'vue'
-import { sanitizeUrl } from '@braintree/sanitize-url'
 
-export const useSanitizeUrl = () => {
-  return url => sanitizeUrl(unref(url))
+import { useLogger } from './useLogger'
+
+export const BLANK_URL = 'about:blank'
+
+export const useSanitizeUrl = (options = {}) => {
+  const {
+    logger = useLogger(),
+    allowedProtocolRegex = /^(?:https?|mailto):$/i,
+  } = options
+
+  return value => {
+    try {
+      const url = new URL(unref(value))
+      if (allowedProtocolRegex.test(url.protocol)) {
+        return url.toString()
+      }
+    } catch (err) {
+      logger.error('Failed to parse URL: %s', err.message)
+    }
+    return BLANK_URL
+  }
 }
