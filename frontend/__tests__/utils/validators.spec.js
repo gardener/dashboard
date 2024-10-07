164de310bf0f8f995fd9da765effc465640c6238
Gross, Lukas <lukas.gross@sap.com>
2024-10-07 16:19:50 +0200

    removed validator test

--- frontend/__tests__/utils/validators.spec.js
+++ frontend/__tests__/utils/validators.spec.js
@@ -22,17 +22,5 @@ describe('utils', () => {
         expect(unique('keys').$validator.call(parentVm, 0)).toBe(true)
       })
     })
-
-    describe('#serviceAccountKey', () => {
-      it('should not validate invalid JSON', () => {
-        expect(serviceAccountKey.$validator('{"valid": false')).toBe(false)
-      })
-      it('should not validate valid JSON with invalid project_id', () => {
-        expect(serviceAccountKey.$validator('{"project_id": "inval!dProjectId"}')).toBe(false)
-      })
-      it('should validate valid JSON with project_id', () => {
-        expect(serviceAccountKey.$validator('{"project_id": "val1d-Pr0ject_ID"}')).toBe(true)
-      })
-    })
   })
 })
