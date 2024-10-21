cc13ba25c7531a5f0341b123b99f8d0b5a7b153d
Gross, Lukas <lukas.gross@sap.com>
2024-10-21 09:23:48 +0200

    import svgs

--- frontend/src/components/GShootEditor.vue
+++ frontend/src/components/GShootEditor.vue
@@ -191,7 +191,11 @@ import GCopyBtn from '@/components/GCopyBtn'
 import GActionButton from '@/components/GActionButton'
 import GAlertBanner from '@/components/GAlertBanner'
 
+import whitespaceEye from '@/assets/whitespace-eye.svg?raw'
+import whitespaceEyeOff from '@/assets/whitespace-eye-off.svg?raw'
+
 import { camelCase } from '@/lodash'
+
 const props = defineProps({
   identifier: {
     type: String,
@@ -253,27 +257,6 @@ const toolbarStyles = computed(() => {
 
 const showToolbar = computed(() => {
   return !isReadOnly.value && !props.hideToolbar
-})
-
-const whitespaceEye = computed(() => {
-  return `
-    <svg viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g id="whitespace-eye" transform="translate(2, 10)" fill="rgb(var(--v-theme-action-button))" fill-rule="nonzero">
-            <path d="M15,6.88 C15.56,6.88 16,7.32 16,7.88 C16,8.44 15.56,8.88 15,8.88 C14.44,8.88 14,8.43 14,7.88 C14,7.33 14.44,6.88 15,6.88 M15,3.88 C17.73,3.88 20.06,5.54 21,7.88 C20.06,10.22 17.73,11.88 15,11.88 C12.27,11.88 9.94,10.22 9,7.88 C9.94,5.54 12.27,3.88 15,3.88 M15,5.38 C13.62,5.38 12.5,6.5 12.5,7.88 C12.5,9.26 13.62,10.38 15,10.38 C16.38,10.38 17.5,9.26 17.5,7.88 C17.5,6.5 16.38,5.38 15,5.38 M0,1.77635684e-15 L0,7 C0,8.1 0.9,9 2,9 L7.42,9 C7.26,8.68 7.12,8.34 7,8 C7.12,7.66 7.26,7.32 7.42,7 L2,7 L2,1.77635684e-15 M16,0 C16,1.37582568 16,2.06373851 16,2.06373851 C16.6954344,2.15253355 17.3688755,2.33212115 18,2.59 L18,0 L16,0 Z" id="Shape"></path>
-        </g>
-    </svg>
-      `
-})
-
-const whitespaceEyeOff = computed(() => {
-  return `
-    <svg viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g id="whitespace-eye-off" transform="translate(1.11, 1.73)" fill="rgb(var(--v-theme-action-button))" fill-rule="nonzero">
-            <path d="M0.89,8.27 L0.89,15.27 C0.89,16.37 1.79,17.27 2.89,17.27 L8.31,17.27 C8.15,16.95 8.01,16.61 7.89,16.27 C8.01,15.93 8.15,15.59 8.31,15.27 L2.89,15.27 L2.89,8.27 L0.89,8.27 Z M16.89,8.27 C16.89,9.64582568 16.89,10.3337385 16.89,10.3337385 C17.5854344,10.4225335 18.2588755,10.6021212 18.89,10.86 L18.89,8.27 L16.89,8.27 Z" id="Shape"></path>
-            <path d="M21.2,17.38 C21.47,17 21.71,16.59 21.89,16.15 C20.96,13.84 18.67,12.19 15.98,12.16 L21.2,17.38 Z M19.73,21 L18.39,19.64 C17.6,19.96 16.77,20.15 15.89,20.15 C13.16,20.15 10.83,18.49 9.89,16.15 C10.34,15.03 11.12,14.07 12.1,13.37 L0,1.27 L1.28,0 L21,19.73 L19.73,21 Z M17.07,18.34 L13.7,14.97 C13.5,15.32 13.39,15.72 13.39,16.15 C13.39,17.53 14.51,18.65 15.89,18.65 C16.32,18.65 16.72,18.54 17.07,18.34 Z" id="Shape"></path>
-        </g>
-    </svg>
-      `
 })
 
 function downloadContent () {
