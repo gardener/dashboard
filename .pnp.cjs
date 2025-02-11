582eaa56a253cf203dd82b67eee27e929b7422a4
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-02-11 15:01:29 +0000

    Update dependency splitpanes to v3.1.8

--- .pnp.cjs
+++ .pnp.cjs
@@ -1866,7 +1866,7 @@ const RAW_RUNTIME_STATE =
           ["sass", "npm:1.79.6"],\
           ["semver", "npm:7.6.3"],\
           ["socket.io-client", "npm:4.8.1"],\
-          ["splitpanes", "npm:3.1.5"],\
+          ["splitpanes", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:3.1.8"],\
           ["statuses", "npm:2.0.1"],\
           ["symbol-tree", "npm:3.2.4"],\
           ["toidentifier", "npm:1.0.1"],\
@@ -12237,10 +12237,23 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["splitpanes", [\
-      ["npm:3.1.5", {\
-        "packageLocation": "./.yarn/cache/splitpanes-npm-3.1.5-ef6ab11cff-de1127143c.zip/node_modules/splitpanes/",\
+      ["npm:3.1.8", {\
+        "packageLocation": "./.yarn/cache/splitpanes-npm-3.1.8-873d75e752-1da043eaa1.zip/node_modules/splitpanes/",\
         "packageDependencies": [\
-          ["splitpanes", "npm:3.1.5"]\
+          ["splitpanes", "npm:3.1.8"]\
+        ],\
+        "linkType": "SOFT"\
+      }],\
+      ["virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:3.1.8", {\
+        "packageLocation": "./.yarn/__virtual__/splitpanes-virtual-57b4605b94/0/cache/splitpanes-npm-3.1.8-873d75e752-1da043eaa1.zip/node_modules/splitpanes/",\
+        "packageDependencies": [\
+          ["splitpanes", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:3.1.8"],\
+          ["@types/vue", null],\
+          ["vue", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:3.5.13"]\
+        ],\
+        "packagePeers": [\
+          "@types/vue",\
+          "vue"\
         ],\
         "linkType": "HARD"\
       }]\
