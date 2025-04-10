554753ea8cbccacddb3274dec090bcb82f229a35
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-04-10 22:30:46 +0000

    Update dependency openid-client to v6.4.2

--- .pnp.cjs
+++ .pnp.cjs
@@ -1649,7 +1649,7 @@ const RAW_RUNTIME_STATE =
           ["neostandard", "virtual:feaa032e1ffbff8da5dad8429b8494744ade8373389ef8e26f3d1f1980ceff327ab996fdc7c1977df285edeb918372fa01d7c87d79c9d7218f8701c70203bfe5#npm:0.12.1"],\
           ["object-hash", "npm:3.0.0"],\
           ["openapi-types", "npm:12.1.3"],\
-          ["openid-client", "npm:6.4.1"],\
+          ["openid-client", "npm:6.4.2"],\
           ["p-event", "npm:4.2.0"],\
           ["p-limit", "npm:3.1.0"],\
           ["p-retry", "npm:4.6.2"],\
@@ -10706,10 +10706,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["oauth4webapi", [\
-      ["npm:3.4.0", {\
-        "packageLocation": "./.yarn/cache/oauth4webapi-npm-3.4.0-d924e57c94-c9ab4c4a74.zip/node_modules/oauth4webapi/",\
+      ["npm:3.4.1", {\
+        "packageLocation": "./.yarn/cache/oauth4webapi-npm-3.4.1-4313f78e2d-d581cfde0d.zip/node_modules/oauth4webapi/",\
         "packageDependencies": [\
-          ["oauth4webapi", "npm:3.4.0"]\
+          ["oauth4webapi", "npm:3.4.1"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -10884,12 +10884,12 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["openid-client", [\
-      ["npm:6.4.1", {\
-        "packageLocation": "./.yarn/cache/openid-client-npm-6.4.1-713ab697cb-55a4d34fcf.zip/node_modules/openid-client/",\
+      ["npm:6.4.2", {\
+        "packageLocation": "./.yarn/cache/openid-client-npm-6.4.2-ed8214f346-a291933d57.zip/node_modules/openid-client/",\
         "packageDependencies": [\
           ["jose", "npm:6.0.10"],\
-          ["oauth4webapi", "npm:3.4.0"],\
-          ["openid-client", "npm:6.4.1"]\
+          ["oauth4webapi", "npm:3.4.1"],\
+          ["openid-client", "npm:6.4.2"]\
         ],\
         "linkType": "HARD"\
       }]\
