3027db50ee51685f64a24e7831e560ee13dba6bc
klocke-io <niklas.klocke@sap.com>
2025-05-07 11:37:57 +0200

    dependency clean up

--- scripts/eslint-yarn-node.mjs
+++ scripts/eslint-yarn-node.mjs
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
-//ToDo license? 
+
 //https://github.com/microsoft/vscode-eslint/issues/1602#issuecomment-1524128714
 
 import { execSync, spawn } from "node:child_process";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
-const scribtDir = dirname(fileURLToPath(import.meta.url))
-console.log('currentDir: ',scribtDir)
-process.chdir(scribtDir);
-
+process.chdir(dirname(fileURLToPath(import.meta.url)));
 
 const nodeOptions = execSync("yarn node -p process.env.NODE_OPTIONS")
   .toString()
