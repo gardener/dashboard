c9bfd63de9b13309d6fa60aa34f353a660b622ad
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-05-12 14:14:06 +0000

    Update dependency @codemirror/view to v6.36.8

--- .pnp.cjs
+++ .pnp.cjs
@@ -1002,7 +1002,7 @@ const RAW_RUNTIME_STATE =
           ["@codemirror/autocomplete", "npm:6.18.6"],\
           ["@codemirror/language", "npm:6.11.0"],\
           ["@codemirror/state", "npm:6.5.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["@lezer/common", "npm:1.2.3"]\
         ],\
         "linkType": "HARD"\
@@ -1015,7 +1015,7 @@ const RAW_RUNTIME_STATE =
           ["@codemirror/commands", "npm:6.8.1"],\
           ["@codemirror/language", "npm:6.11.0"],\
           ["@codemirror/state", "npm:6.5.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["@lezer/common", "npm:1.2.3"]\
         ],\
         "linkType": "HARD"\
@@ -1043,7 +1043,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["@codemirror/language", "npm:6.11.0"],\
           ["@codemirror/state", "npm:6.5.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["@lezer/common", "npm:1.2.3"],\
           ["@lezer/highlight", "npm:1.2.1"],\
           ["@lezer/lr", "npm:1.4.2"],\
@@ -1058,7 +1058,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["@codemirror/search", "npm:6.5.10"],\
           ["@codemirror/state", "npm:6.5.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["crelt", "npm:1.0.6"]\
         ],\
         "linkType": "HARD"\
@@ -1081,18 +1081,18 @@ const RAW_RUNTIME_STATE =
           ["@codemirror/language", "npm:6.11.0"],\
           ["@codemirror/state", "npm:6.5.2"],\
           ["@codemirror/theme-one-dark", "npm:6.1.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["@lezer/highlight", "npm:1.2.1"]\
         ],\
         "linkType": "HARD"\
       }]\
     ]],\
     ["@codemirror/view", [\
-      ["npm:6.36.7", {\
-        "packageLocation": "./.yarn/cache/@codemirror-view-npm-6.36.7-bde92c4790-243d57e591.zip/node_modules/@codemirror/view/",\
+      ["npm:6.36.8", {\
+        "packageLocation": "./.yarn/cache/@codemirror-view-npm-6.36.8-f1a3a9e52a-0ec264fb6f.zip/node_modules/@codemirror/view/",\
         "packageDependencies": [\
           ["@codemirror/state", "npm:6.5.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["style-mod", "npm:4.1.2"],\
           ["w3c-keyname", "npm:2.2.8"]\
         ],\
@@ -1698,7 +1698,7 @@ const RAW_RUNTIME_STATE =
           ["@codemirror/search", "npm:6.5.10"],\
           ["@codemirror/state", "npm:6.5.2"],\
           ["@codemirror/theme-one-dark", "npm:6.1.2"],\
-          ["@codemirror/view", "npm:6.36.7"],\
+          ["@codemirror/view", "npm:6.36.8"],\
           ["@fontsource/roboto", "npm:5.2.5"],\
           ["@gardener-dashboard/frontend", "workspace:frontend"],\
           ["@kyvg/vue3-notification", "virtual:8d919ffb8fd728f827df3f6a566e8e923223ffcec68f7450d83bbbc2dc25d6b8c987e111cbab484b209f253bdf2f2e00663b01a986262c44511128466462a76f#npm:3.4.1"],\
