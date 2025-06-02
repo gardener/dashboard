d7957ce2f77893efc4e4a4631087f15ed7a2885a
Gross, Lukas <lukas.gross@sap.com>
2025-06-02 12:25:55 +0200

    Always show cloudprofile

--- frontend/src/components/NewShoot/GNewShootInfrastructureDetails.vue
+++ frontend/src/components/NewShoot/GNewShootInfrastructureDetails.vue
@@ -7,10 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 <template>
   <v-container class="px-0 mx-0">
     <v-row>
-      <v-col
-        v-if="cloudProfiles.length > 1 || !cloudProfileHasSeedNames"
-        cols="3"
-      >
+      <v-col cols="3">
         <g-select-cloud-profile
           ref="cloudProfile"
           v-model="cloudProfileRef"
@@ -340,15 +337,6 @@ export default {
           },
         }
       })
-    },
-    cloudProfileHasSeedNames () {
-      if (this.cloudProfileRef?.kind === 'CloudProfile') {
-        const selectedCloudProfile = find(this.cloudProfiles, { metadata: { name: this.cloudProfileRef.name } })
-        return selectedCloudProfile?.data.seedNames?.length
-      } else {
-        // TODO: Implement this for NamespacedCloudProfile
-        return false
-      }
     },
   },
   mounted () {
