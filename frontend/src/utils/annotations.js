ce35db1ab7515e3cdd73b7e8391b5c932937b6f5
Marc Vornetran <marc.vornetran@sap.com>
2025-06-03 11:57:07 +0200

    feat: Add constant annotation utility

---
+++ frontend/src/utils/annotations.js
@@ -0,0 +1,9 @@
+//
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+export const annotations = Object.freeze({
+  projectTitle: 'dashboard.gardener.cloud/project-title',
+})
