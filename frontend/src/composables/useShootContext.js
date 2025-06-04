df465a542a3be7bbf246feda8906f09810233aa3
Benedikt Haug <benedikt.haug@noris.de>
2025-06-04 09:26:32 +0200

    add stuff

--- frontend/src/composables/useShootContext.js
+++ frontend/src/composables/useShootContext.js
@@ -877,7 +877,6 @@ export function createShootContextComposable (options = {}) {
       return !!controlPlaneHighAvailabilityFailureToleranceType.value
     },
     set (value) {
-      console.log("bla")
       if (!value) {
         controlPlaneHighAvailabilityFailureToleranceType.value = undefined
       } else {
