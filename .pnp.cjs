a08f204881c910e9fc3f68ffbc1f3b45ed029ec0
gardener-ci-robot <gardener.ci.robot@gmail.com>
2025-07-11 00:34:20 +0000

    Update octokit monorepo

--- .pnp.cjs
+++ .pnp.cjs
@@ -1648,8 +1648,8 @@ const RAW_RUNTIME_STATE =
           ["@gardener-dashboard/test-utils", "workspace:packages/test-utils"],\
           ["@godaddy/terminus", "npm:4.12.1"],\
           ["@microsoft/eslint-formatter-sarif", "npm:3.1.0"],\
-          ["@octokit/auth-app", "npm:6.1.3"],\
-          ["@octokit/core", "npm:5.2.1"],\
+          ["@octokit/auth-app", "npm:6.1.4"],\
+          ["@octokit/core", "npm:5.2.2"],\
           ["@octokit/plugin-paginate-graphql", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:4.0.1"],\
           ["@octokit/plugin-paginate-rest", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:9.2.2"],\
           ["@octokit/plugin-request-log", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:4.0.1"],\
@@ -2802,10 +2802,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@octokit/auth-app", [\
-      ["npm:6.1.3", {\
-        "packageLocation": "./.yarn/cache/@octokit-auth-app-npm-6.1.3-e297167f83-aa2a769327.zip/node_modules/@octokit/auth-app/",\
+      ["npm:6.1.4", {\
+        "packageLocation": "./.yarn/cache/@octokit-auth-app-npm-6.1.4-b506cdb2fa-8b53420b7b.zip/node_modules/@octokit/auth-app/",\
         "packageDependencies": [\
-          ["@octokit/auth-app", "npm:6.1.3"],\
+          ["@octokit/auth-app", "npm:6.1.4"],\
           ["@octokit/auth-oauth-app", "npm:7.1.0"],\
           ["@octokit/auth-oauth-user", "npm:4.1.0"],\
           ["@octokit/request", "npm:8.4.1"],\
@@ -2876,11 +2876,11 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@octokit/core", [\
-      ["npm:5.2.1", {\
-        "packageLocation": "./.yarn/cache/@octokit-core-npm-5.2.1-383a3c951b-9759c70a6a.zip/node_modules/@octokit/core/",\
+      ["npm:5.2.2", {\
+        "packageLocation": "./.yarn/cache/@octokit-core-npm-5.2.2-c3527b99ef-b4484d8555.zip/node_modules/@octokit/core/",\
         "packageDependencies": [\
           ["@octokit/auth-token", "npm:4.0.0"],\
-          ["@octokit/core", "npm:5.2.1"],\
+          ["@octokit/core", "npm:5.2.2"],\
           ["@octokit/graphql", "npm:7.1.0"],\
           ["@octokit/request", "npm:8.4.1"],\
           ["@octokit/request-error", "npm:5.1.1"],\
@@ -2964,7 +2964,7 @@ const RAW_RUNTIME_STATE =
       ["virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:4.0.1", {\
         "packageLocation": "./.yarn/__virtual__/@octokit-plugin-paginate-graphql-virtual-5c6bb13512/0/cache/@octokit-plugin-paginate-graphql-npm-4.0.1-83b19b5121-d559cdc2b5.zip/node_modules/@octokit/plugin-paginate-graphql/",\
         "packageDependencies": [\
-          ["@octokit/core", "npm:5.2.1"],\
+          ["@octokit/core", "npm:5.2.2"],\
           ["@octokit/plugin-paginate-graphql", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:4.0.1"],\
           ["@types/octokit__core", null]\
         ],\
@@ -2986,7 +2986,7 @@ const RAW_RUNTIME_STATE =
       ["virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:9.2.2", {\
         "packageLocation": "./.yarn/__virtual__/@octokit-plugin-paginate-rest-virtual-a79b8e04ec/0/cache/@octokit-plugin-paginate-rest-npm-9.2.2-1b0382bc4a-e9c85b1706.zip/node_modules/@octokit/plugin-paginate-rest/",\
         "packageDependencies": [\
-          ["@octokit/core", "npm:5.2.1"],\
+          ["@octokit/core", "npm:5.2.2"],\
           ["@octokit/plugin-paginate-rest", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:9.2.2"],\
           ["@octokit/types", "npm:12.6.0"],\
           ["@types/octokit__core", null]\
@@ -3009,7 +3009,7 @@ const RAW_RUNTIME_STATE =
       ["virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:4.0.1", {\
         "packageLocation": "./.yarn/__virtual__/@octokit-plugin-request-log-virtual-1d304a2a4a/0/cache/@octokit-plugin-request-log-npm-4.0.1-bc9d8da84b-6f556f8625.zip/node_modules/@octokit/plugin-request-log/",\
         "packageDependencies": [\
-          ["@octokit/core", "npm:5.2.1"],\
+          ["@octokit/core", "npm:5.2.2"],\
           ["@octokit/plugin-request-log", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:4.0.1"],\
           ["@types/octokit__core", null]\
         ],\
@@ -3031,7 +3031,7 @@ const RAW_RUNTIME_STATE =
       ["virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:10.4.1", {\
         "packageLocation": "./.yarn/__virtual__/@octokit-plugin-rest-endpoint-methods-virtual-292623c079/0/cache/@octokit-plugin-rest-endpoint-methods-npm-10.4.1-3fc380dbdf-4b8f64c0f7.zip/node_modules/@octokit/plugin-rest-endpoint-methods/",\
         "packageDependencies": [\
-          ["@octokit/core", "npm:5.2.1"],\
+          ["@octokit/core", "npm:5.2.2"],\
           ["@octokit/plugin-rest-endpoint-methods", "virtual:91da830b29af2704bfc9679729fb85d00ca0b8eeb24a837747a5bc0b5aec0e922594580a901a48c34c06a65b9b376f23fa3cbb88dc9fd35cddc5076ab48a067f#npm:10.4.1"],\
           ["@octokit/types", "npm:12.6.0"],\
           ["@types/octokit__core", null]\
