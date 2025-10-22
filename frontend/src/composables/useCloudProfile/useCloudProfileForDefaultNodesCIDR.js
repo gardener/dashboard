50c65d9defe3bcc1c267a76cdeb6a58c4f99f547
klocke-io <niklas.klocke@sap.com>
2025-10-22 16:35:48 +0200

    refactor(frontend): extract single use functions - Extracted `seedsByCloudProfileRef` to consumers - Extracted `getDefaultNodesCIDR` to `useCloudProfileForDefaultNodesCIDR` composable - Moved `generateWorker` and `expiringWorkerGroupsForShoot` to `useShootContext` and `useShootMessages` composables respectively - Removed obsolete tests and adapted existing tests to the new composable structure

---
+++ frontend/src/composables/useCloudProfile/useCloudProfileForDefaultNodesCIDR.js
@@ -0,0 +1,39 @@
+//
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import {
+  computed,
+  isRef,
+} from 'vue'
+
+import { useConfigStore } from '@/store/config'
+
+import get from 'lodash/get'
+
+/**
+ * Composable for getting default nodes CIDR from cloud profile
+ * @param {Ref<CloudProfile>} cloudProfile - Vue ref containing the cloud profile object
+ * @returns {Object} Object containing computed property for default nodes CIDR
+ */
+export function useCloudProfileForDefaultNodesCIDR (cloudProfile) {
+  if (!isRef(cloudProfile)) {
+    throw new Error('cloudProfile must be a ref!')
+  }
+
+  const configStore = useConfigStore()
+
+  const defaultNodesCIDR = computed(() => {
+    return get(
+      cloudProfile.value,
+      ['spec', 'providerConfig', 'defaultNodesCIDR'],
+      configStore.defaultNodesCIDR,
+    )
+  })
+
+  return {
+    defaultNodesCIDR,
+  }
+}
