86938fe6703ab8d1a63877339c8129e7aab01d76
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2026-01-23 13:15:42 +0100

    Update Yarn to v4.12.0 (#2660)
    * Update Yarn to v4.12.0
    
    * yarn
    
    * bump yarn version
    
    ---------
    
    Co-authored-by: Peter Sutter <peter.sutter@sap.com>

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -57,7 +57,7 @@ module.exports = {
     enforceFieldsOnAllWorkspaces(ctx, {
       license: 'Apache-2.0',
       'engines.node': '22.18.0',
-      'packageManager': 'yarn@4.9.4',
+      'packageManager': 'yarn@4.12.0',
       'repository.type': 'git',
       'repository.url': 'git+https://github.com/gardener/dashboard.git',
       'repository.directory': workspace => workspace.cwd,
