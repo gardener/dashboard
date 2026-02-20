0d8b9b069d2f2df7022a5fc15c0174d939c9c464
Lukas Gross <lukas.gross@sap.com>
2026-02-20 08:41:20 +0100

    Update undici to 7.22.0 (#2789)

--- .pnp.cjs
+++ .pnp.cjs
@@ -1726,7 +1726,7 @@ const RAW_RUNTIME_STATE =
           ["socket.io", "npm:4.8.1"],\
           ["socket.io-client", "npm:4.8.1"],\
           ["supertest", "npm:7.1.4"],\
-          ["undici", "npm:7.15.0"],\
+          ["undici", "npm:7.22.0"],\
           ["unified", "npm:11.0.5"],\
           ["uuid", "npm:11.1.0"]\
         ],\
@@ -15122,10 +15122,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["undici", [\
-      ["npm:7.15.0", {\
-        "packageLocation": "./.yarn/cache/undici-npm-7.15.0-c258df41d8-2c526bf157.zip/node_modules/undici/",\
+      ["npm:7.22.0", {\
+        "packageLocation": "./.yarn/cache/undici-npm-7.22.0-87832cba3b-09777c06f3.zip/node_modules/undici/",\
         "packageDependencies": [\
-          ["undici", "npm:7.15.0"]\
+          ["undici", "npm:7.22.0"]\
         ],\
         "linkType": "HARD"\
       }]\
