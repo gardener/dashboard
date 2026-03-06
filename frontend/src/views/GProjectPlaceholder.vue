58822d040d9331efc7857b2ea6ee31c639475cf9
Peter Sutter <peter.sutter@sap.com>
2026-03-06 15:20:16 +0100

    refactor: use useItemPlaceHolder in GProjectPlaceholder

--- frontend/src/views/GProjectPlaceholder.vue
+++ frontend/src/views/GProjectPlaceholder.vue
@@ -14,28 +14,19 @@ SPDX-License-Identifier: Apache-2.0
 import {
   ref,
   computed,
-  watch,
-  onBeforeMount,
-  onMounted,
-  onBeforeUnmount,
   toRef,
 } from 'vue'
-import {
-  useRoute,
-  onBeforeRouteLeave,
-  onBeforeRouteUpdate,
-} from 'vue-router'
+import { useRoute } from 'vue-router'
 
 import { useAuthzStore } from '@/store/authz'
 import { useProjectStore } from '@/store/project'
 
 import GProjectError from '@/views/GProjectError.vue'
 
-import isEqual from 'lodash/isEqual'
-
-function isLoadRequired (route, to) {
-  return route.name !== to.name || !isEqual(route.params, to.params)
-}
+import {
+  isLoadRequired,
+  useItemPlaceholder,
+} from '@/composables/useItemPlaceholder'
 
 export default {
   components: {
@@ -56,62 +47,32 @@ export default {
     const project = toRef(projectStore, 'project')
     const namespace = toRef(projectStore, 'namespace')
 
-    const readyState = ref('initial')
-    const error = ref(null)
     const fallbackRoute = ref(null)
 
-    const component = computed(() => {
-      if (error.value) {
-        return 'g-project-error'
-      }
-      return 'router-view'
-    })
+    const projectItem = computed(() => namespace.value === '_all' || project.value)
 
-    const componentProperties = computed(() => {
-      switch (component.value) {
-        case 'g-project-error': {
-          const {
-            code = 500,
-            reason = 'Oops, something went wrong',
-            message = 'An unexpected error occurred. Please try again later',
-          } = error.value ?? {}
-          return {
-            code,
-            text: reason,
-            message,
-            fallbackRoute: fallbackRoute.value,
-          }
-        }
-        default: {
-          return {}
-        }
-      }
-    })
-
-    function load (route) {
-      error.value = null
+    function load (route, { setError }) {
       fallbackRoute.value = null
-      readyState.value = 'loading'
       const routeName = route.name
       const routeParams = route.params
       if (authzStore.namespace !== routeParams.namespace) {
-        error.value = new Error('An unexpected error occurred')
+        setError(new Error('An unexpected error occurred'))
         fallbackRoute.value = {
           name: 'Home',
         }
       } else if (!projectStore.namespaces.includes(authzStore.namespace) && authzStore.namespace !== '_all') {
-        error.value = Object.assign(new Error('The project you are looking for doesn\'t exist or you are not authorized to view this project!'), {
+        setError(Object.assign(new Error('The project you are looking for doesn\'t exist or you are not authorized to view this project!'), {
           code: 404,
           reason: 'Project not found',
-        })
+        }))
         fallbackRoute.value = {
           name: 'Home',
         }
       } else if (['Secrets', 'Secret'].includes(routeName) && !authzStore.canGetCloudProviderCredentials) {
-        error.value = Object.assign(new Error('You do not have the necessary permissions to list secrets!'), {
+        setError(Object.assign(new Error('You do not have the necessary permissions to list secrets!'), {
           code: 403,
           reason: 'Forbidden',
-        })
+        }))
         fallbackRoute.value = {
           name: 'ShootList',
           params: {
@@ -121,55 +82,41 @@ export default {
       }
     }
 
-    onBeforeMount(() => {
-      readyState.value = 'initial'
+    const {
+      component,
+      componentProperties: itemPlaceholderComponentProperties,
+      load: loadRoute,
+    } = useItemPlaceholder({
+      route,
+      item: projectItem,
+      load,
+      errorComponent: 'g-project-error',
+      loadingComponent: 'div',
+      getGoneError: () => {
+        fallbackRoute.value = {
+          name: 'Home',
+        }
+        return Object.assign(new Error('The project you are looking for is no longer available'), {
+          code: 410,
+          reason: 'Project is gone',
+        })
+      },
     })
 
-    onMounted(async () => {
-      await load(route)
-      readyState.value = 'loaded'
-    })
-
-    onBeforeUnmount(() => {
-      readyState.value = 'initial'
-    })
-
-    onBeforeRouteUpdate(async to => {
-      if (isLoadRequired(route, to)) {
-        await load(to)
-      }
-    })
-
-    onBeforeRouteLeave(() => {
-      readyState.value = 'initial'
-    })
-
-    watch(() => route.path, value => {
-      if (value) {
-        readyState.value = 'loaded'
-      }
-    })
-
-    watch(project, value => {
-      if (readyState.value === 'loaded') {
-        if (!value && namespace.value !== '_all') {
-          fallbackRoute.value = {
-            name: 'Home',
-          }
-          error.value = Object.assign(new Error('The project you are looking for is no longer available'), {
-            code: 410,
-            reason: 'Project is gone',
-          })
-        } else if ([404, 410].includes(error.value?.code)) {
-          fallbackRoute.value = null
-          error.value = null
+    const componentProperties = computed(() => {
+      if (component.value === 'g-project-error') {
+        return {
+          ...itemPlaceholderComponentProperties.value,
+          fallbackRoute: fallbackRoute.value,
         }
       }
+      return itemPlaceholderComponentProperties.value
     })
 
     return {
       component,
       componentProperties,
+      load: loadRoute,
     }
   },
 }
