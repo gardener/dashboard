a0731cb74f413003ea5a7e43c46da33b91927d00
Lukas Gross <lukas.gross@sap.com>
2026-04-15 19:35:33 +0200

    Added missing headers

--- scripts/vitest-node.sh
+++ scripts/vitest-node.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 set -euo pipefail
 workspace_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 export NODE_OPTIONS="--require ${workspace_dir}/.pnp.cjs ${NODE_OPTIONS-}"
