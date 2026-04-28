450d91e8dc6413ff92ba7543193274e9437a0002
Lukas Gross <lukas.gross@sap.com>
2026-04-28 10:18:23 +0200

    Updated Breadcrumb title styling to better match previous styling

--- frontend/src/components/GBreadcrumb.vue
+++ frontend/src/components/GBreadcrumb.vue
@@ -18,7 +18,7 @@ SPDX-License-Identifier: Apache-2.0
       />
     </template>
     <template #title="{ item }">
-      <span :class="{ 'text-title-large': !item.to }">
+      <span :class="{ 'text-title-large ml-3': !item.to }">
         {{ item.title || item }}
       </span>
     </template>
