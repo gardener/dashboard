924d74a298b9486ad19e951353a66bcfbc956c8d
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-05-16 14:07:21 +0000

    Update dependency sanitize-html to v2.17.4

--- .pnp.cjs
+++ .pnp.cjs
@@ -954,7 +954,7 @@ const RAW_RUNTIME_STATE =
           ["remark-github", "npm:12.0.0"],\
           ["remark-parse", "npm:11.0.0"],\
           ["remark-rehype", "npm:11.1.2"],\
-          ["sanitize-html", "npm:2.17.3"],\
+          ["sanitize-html", "npm:2.17.4"],\
           ["semver", "npm:7.7.4"],\
           ["set-cookie-parser", "npm:2.7.1"],\
           ["socket.io", "npm:4.8.3"],\
@@ -8448,6 +8448,16 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["launder", [\
+      ["npm:1.7.1", {\
+        "packageLocation": "./.yarn/cache/launder-npm-1.7.1-8f3056904f-c4884c08cc.zip/node_modules/launder/",\
+        "packageDependencies": [\
+          ["dayjs", "npm:1.11.20"],\
+          ["launder", "npm:1.7.1"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["levn", [\
       ["npm:0.4.1", {\
         "packageLocation": "./.yarn/cache/levn-npm-0.4.1-d183b2d7bb-effb03cad7.zip/node_modules/levn/",\
@@ -10864,16 +10874,17 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["sanitize-html", [\
-      ["npm:2.17.3", {\
-        "packageLocation": "./.yarn/cache/sanitize-html-npm-2.17.3-ab76a9fb99-8afa59bed1.zip/node_modules/sanitize-html/",\
+      ["npm:2.17.4", {\
+        "packageLocation": "./.yarn/cache/sanitize-html-npm-2.17.4-f9f9eb85a7-5c352376a4.zip/node_modules/sanitize-html/",\
         "packageDependencies": [\
           ["deepmerge", "npm:4.3.1"],\
           ["escape-string-regexp", "npm:4.0.0"],\
           ["htmlparser2", "npm:10.1.0"],\
           ["is-plain-object", "npm:5.0.0"],\
+          ["launder", "npm:1.7.1"],\
           ["parse-srcset", "npm:1.0.2"],\
           ["postcss", "npm:8.5.10"],\
-          ["sanitize-html", "npm:2.17.3"]\
+          ["sanitize-html", "npm:2.17.4"]\
         ],\
         "linkType": "HARD"\
       }]\
