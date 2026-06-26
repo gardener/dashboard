1c02068cb014b52df1e7749f38a1f7213d34be16
Lukas Gross <lukas.gross@sap.com>
2026-06-26 13:44:46 +0200

    CodexFeedback #1 - Fixed some wrong provider secret keys - Fixed parsing of structured input fields is no explicit

--- frontend/src/data/vendors/infra/alicloud.js
+++ frontend/src/data/vendors/infra/alicloud.js
@@ -94,7 +94,7 @@ export default {
         },
       },
       {
-        key: 'secretAccessKey',
+        key: 'accessKeySecret',
         label: 'Access Key Secret',
         hint: 'e.g. WJalrXUtnFEMIK7MDENG/bPxRfiCYz',
         type: 'text',
