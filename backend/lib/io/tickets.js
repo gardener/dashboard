7e4d727806218cdb21982eeeaa4f2856f677783d
Julia Baum <julia.baum@sap.com>
2026-07-07 10:41:39 +0200

    Add license header

--- backend/lib/io/tickets.js
+++ backend/lib/io/tickets.js
@@ -1,3 +1,9 @@
+//
+// SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
 import createError from 'http-errors'
 import cache from '../cache/index.js'
 import * as authorization from '../services/authorization.js'
