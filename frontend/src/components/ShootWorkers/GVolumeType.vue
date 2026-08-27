d7cb3f16076ff01855fcb414d537375c95ac4a3b
julestree <julia.baum@sap.com>
2026-08-27 14:17:02 +0200

    fix(workers): reserve hint space on volume type select
    Signed-off-by: julestree <julia.baum@sap.com>

--- frontend/src/components/ShootWorkers/GVolumeType.vue
+++ frontend/src/components/ShootWorkers/GVolumeType.vue
@@ -18,6 +18,7 @@ SPDX-License-Identifier: Apache-2.0
       :error-messages="getErrorMessages(v$.worker.volume.type)"
       label="Volume Type"
       :hint="hint"
+      persistent-hint
       persistent-placeholder
       variant="underlined"
       @update:model-value="onInputVolumeType"
