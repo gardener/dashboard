6b2e5690752b92f6dd738b93a72c6b4664b9215c
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-09-01 12:31:00 +0000

    Update dependency morgan to v1.12.0
    Signed-off-by: gardener-ci-robot <gardener.ci.robot@gmail.com>

--- .pnp.cjs
+++ .pnp.cjs
@@ -964,7 +964,7 @@ const RAW_RUNTIME_STATE =
           ["jsonwebtoken", "npm:9.0.3"],\
           ["lodash", "npm:4.18.1"],\
           ["lodash-es", "npm:4.18.1"],\
-          ["morgan", "npm:1.11.0"],\
+          ["morgan", "npm:1.12.0"],\
           ["neostandard", "virtual:f3f18773c1f2811e8d448670abfc3fed18cdffc11b444f7cbc3548ae5868e74f3c4ee449327c1fc9c24ce0732ee02505411a07539789bec8257188d17bbada1f#npm:0.13.0"],\
           ["object-hash", "npm:3.0.0"],\
           ["openapi-types", "npm:12.1.3"],\
@@ -10048,13 +10048,13 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["morgan", [\
-      ["npm:1.11.0", {\
-        "packageLocation": "./.yarn/cache/morgan-npm-1.11.0-348cdbbc68-1d8c67bdeb.zip/node_modules/morgan/",\
+      ["npm:1.12.0", {\
+        "packageLocation": "./.yarn/cache/morgan-npm-1.12.0-fcc384b2b9-84480e2414.zip/node_modules/morgan/",\
         "packageDependencies": [\
           ["basic-auth", "npm:2.0.1"],\
           ["debug", "virtual:e34a5db40400b9e5b2ff4a99207e6e6259d3054982622ae91b68c81eb2631164c6d9db4ab29c0dbf7620574b8d6b678f010601eafc55a6b832550053c1b61b7b#npm:2.6.9"],\
           ["depd", "npm:2.0.0"],\
-          ["morgan", "npm:1.11.0"],\
+          ["morgan", "npm:1.12.0"],\
           ["on-finished", "npm:2.4.1"],\
           ["on-headers", "npm:1.1.0"]\
         ],\
