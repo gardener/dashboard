bb36d28a1288eebad473b76c585f1b3de1850897
julestree <julia.baum@sap.com>
2026-09-07 14:07:10 +0200

    remove added ResizeObserver from readonly globals
    Signed-off-by: julestree <julia.baum@sap.com>

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -189,7 +189,6 @@ module.exports = [
       MouseEvent: 'readonly',
       FileReader: 'readonly',
       Range: 'readonly',
-      ResizeObserver: 'readonly',
       fixtures: 'readonly',
       requestAnimationFrame: 'readonly',
     },
