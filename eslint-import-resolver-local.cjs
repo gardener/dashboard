9c0ba71414e059e13ff7ef6ce85a8589aeae5bf1
Lukas Gross <lukas.gross@sap.com>
2026-09-08 13:28:18 +0200

    chore(eslint): validate imports in packages
    Enable the recommended import rules for all package workspaces and
    resolve dependencies through Yarn PnP.
    
    Signed-off-by: Lukas Gross <lukas.gross@sap.com>

--- eslint-import-resolver-local.cjs
+++ eslint-import-resolver-local.cjs
@@ -4,15 +4,35 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+const { createRequire, isBuiltin } = require('node:module')
+
 module.exports = {
   interfaceVersion: 2,
-  resolve (source, file, options) {
+  resolve (source, file, options = {}) {
     const { map: mapEntries = [] } = options
     const map = new Map(mapEntries)
-    const found = map.has(source)
-    const path = found
-      ? map.get(source)
-      : undefined
-    return { found, path }
+
+    if (map.has(source)) {
+      return {
+        found: true,
+        path: map.get(source),
+      }
+    }
+
+    if (isBuiltin(source)) {
+      return {
+        found: true,
+        path: null,
+      }
+    }
+
+    try {
+      return {
+        found: true,
+        path: createRequire(file).resolve(source),
+      }
+    } catch {
+      return { found: false }
+    }
   },
 }
