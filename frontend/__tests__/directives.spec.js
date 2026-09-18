39f7ebddd194e5ca373ebbd08cd99e0809750fc4
julestree <julia.baum@sap.com>
2026-09-18 15:03:38 +0200

    remove false-positive style attribute test
    DOMPurify does not work 100% with jsdom rendering and therefore the test rendered a style attribute in a case where the browser wouldn't.
    
    Signed-off-by: julestree <julia.baum@sap.com>

--- frontend/__tests__/directives.spec.js
+++ frontend/__tests__/directives.spec.js
@@ -172,10 +172,5 @@ describe('directives', () => {
       await wrapper.setData({ content: '<i>second</i>' })
       expect(wrapper.find('div').element.innerHTML).toBe('<i>second</i>')
     })
-
-    it('preserves inline style attributes', () => {
-      const wrapper = mountWithSafeHtml('<p style="color: red;">text</p>')
-      expect(wrapper.find('div').element.innerHTML).toContain('style="color: red;"')
-    })
   })
 })
